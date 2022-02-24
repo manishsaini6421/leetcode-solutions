fc03c6c7219784f9fd82c58abf8b1c68cd8b488f
manishsaini6421 <57342164+manishsaini6421@users.noreply.github.com>
2022-02-24 11:51:27 +0530

    Time: 45 ms (13.54%), Space: 20 MB (13.34%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       map<int,int> m;
+       unordered_map<int,int> m;
         int ans;
     for(int i=0; i<nums.size(); i++){
         m[nums[i]]=m[nums[i]]+1;
