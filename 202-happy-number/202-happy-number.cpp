70178857dd57122465d55af9a06e9669b433104f
manishsaini6421 <57342164+manishsaini6421@users.noreply.github.com>
2022-02-28 16:31:39 +0530

    Time: 48 ms (6.05%), Space: 5.9 MB (85.16%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int t=1000;
+        while(n!=1 && t>0){
+            string s=to_string(n);
+            n=0;
+            for(int i=0; i<s.size(); i++){
+                n+=(s[i]-'0')*(s[i]-'0');
+            }
+           t--;
+        }
+        if(n==1)
+            return true;
+        return false;
+    }
+};
