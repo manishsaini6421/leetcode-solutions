5e99dcb73dcf3af4000e4d44accce5937f9bf23b
manishsaini6421 <57342164+manishsaini6421@users.noreply.github.com>
2022-06-04 04:44:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.52%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
+    vector<int> v;
     int tribonacci(int n) {
-        int memo[38];
-        memo[0]=0;
-        memo[1]=1;
-        memo[2]=1;
-        for(int i=3; i<=37; i++){
-            memo[i]=memo[i-1]+memo[i-2]+memo[i-3];
+        v.push_back(0);
+        v.push_back(1);
+        v.push_back(1);
+        for(int i=3; i<=n; i++){
+            v.push_back(v[i-1]+v[i-2]+v[i-3]);
         }
-          
-        return memo[n];
+        return v[n];
     }
 };
