2641051a1a1b0c7a5c7ec9bf82df3e33fd3ac7d9
manishsaini6421 <57342164+manishsaini6421@users.noreply.github.com>
2022-06-04 04:42:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.08%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+    vector<int> v;
+ 
     int fib(int n) {
-        int memo[31];
-        memo[0]=0;
-        memo[1]=1;
-        for(int i=2; i<=30; i++){
-            memo[i]=memo[i-1]+memo[i-2];
-        }
-        return memo[n];
+        v.push_back(0);
+        v.push_back(1);
+       for(int i=2; i<=n; i++){
+           v.push_back(v[i-1]+v[i-2]);
+       }
+       return v[n];
     }
 };
