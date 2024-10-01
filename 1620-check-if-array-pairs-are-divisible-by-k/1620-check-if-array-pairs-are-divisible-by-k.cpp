14fbc446dd7556d2b313d1876ea015bb8c521993
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-02 01:39:46 +0530

    Time: 279 ms (5.13%), Space: 84 MB (5.39%) - LeetHub

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        if(arr.size()==0) return false;
+        vector<vector<int>> store(k);
+       for(int i=0; i<arr.size(); i++){
+            cout<<i<<endl;
+            int a;
+            if(arr[i]<0){
+                if(abs(arr[i])%k==0)
+                    a=0;
+                else{
+                    a=k-(abs(arr[i])%k);
+                    
+                }  
+                store[a].push_back(arr[i]);  
+                
+            }
+            else{
+                
+                a=arr[i]%k;
+                store[a].push_back(arr[i]);
+                
+            }
+       }     
+        
+        if(store[0].size()%2!=0)
+            return false;
+        int i=1,j=store.size()-1;    
+        while(i<j){
+            if(store[i].size()!=store[j].size())
+                return false;
+            i++;
+            j--;    
+        }
+        if(k%2==0 && (store[k/2].size())%2!=0)
+            return false;
+       
+        return true;
+    }
+};
