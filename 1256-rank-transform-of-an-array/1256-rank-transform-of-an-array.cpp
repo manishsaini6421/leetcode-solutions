63a630956356df1e3a558c4edefef3416732fcaa
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-02 12:51:27 +0530

    Time: 61 ms (79.79%), Space: 45.7 MB (22.93%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        
+        vector<int> res;
         vector<int> temp;
-        if(arr.size()==0) return temp;
+
+        if(arr.size()==0) return res;
         for(auto num: arr){
             temp.push_back(num);
         }
         sort(temp.begin(),temp.end());
+
         unordered_map<int,int> map;
         int j=1;
         map[temp[0]]=1;
         for(int i=1; i<temp.size(); i++){
-            if(temp[i-1]==temp[i])
-                map[temp[i]]=j;
-            else
-                j++;
+            if(temp[i-1]!=temp[i])
+               j++;
             map[temp[i]]=j;
         }
-        vector<int> res;
+
+       
         for(int i=0; i<arr.size(); i++){
             res.push_back(map[arr[i]]);
         }
