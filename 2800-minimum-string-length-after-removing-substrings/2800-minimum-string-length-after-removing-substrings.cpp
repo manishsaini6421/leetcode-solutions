ab8beb5ce2f640379d76ff10b89558917c3f9521
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-07 22:23:29 +0530

    Time: 0 ms (100%), Space: 12.1 MB (11.13%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -5,9 +5,8 @@ public:
         int i=0;
         while(i<s.size()){
             if(!st.empty() && ((st.top()=='A' && s[i]=='B') || (st.top()=='C' && s[i]=='D')))st.pop();
-            else{
-                st.push(s[i]);
-            }
+            else st.push(s[i]);
+            
            i++;
         }
         return st.size();
