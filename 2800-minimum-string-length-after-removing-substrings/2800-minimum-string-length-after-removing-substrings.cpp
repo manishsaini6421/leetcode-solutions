f5a399aabdc780247f4c1bcd01ada2dfd5ad7360
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-07 22:09:48 +0530

    Time: 3 ms (89.38%), Space: 10 MB (97.42%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -8,13 +8,7 @@ public:
                 s.erase(i,2);
                 if(i!=0) i--;
             }
-            // else if((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D')){
-            //     s.erase(i,2);
-            //     i--;
-                
-            // }
-            
-           else i++;
+            else i++;
            
         }
         return s.size();
