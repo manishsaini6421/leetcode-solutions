5fab66db6448c56c39fa62a3f4e31477cf7f9ea9
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-07 22:08:51 +0530

    Time: 7 ms (51.96%), Space: 9.9 MB (97.42%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -3,14 +3,16 @@ public:
     int minLength(string s) {
         int i=0;
         while(i<s.size()){
-            if(i==0 && ((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D'))){
-                s.erase(i,2);
-            }
-            else if((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D')){
-                s.erase(i,2);
-                i--;
+            if((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D')){
                 
+                s.erase(i,2);
+                if(i!=0) i--;
             }
+            // else if((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D')){
+            //     s.erase(i,2);
+            //     i--;
+                
+            // }
             
            else i++;
            
