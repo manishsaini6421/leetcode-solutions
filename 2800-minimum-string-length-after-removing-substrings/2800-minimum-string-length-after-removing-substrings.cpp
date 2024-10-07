8610e4a7baa13c475e36554d726184baf5980525
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-07 22:06:35 +0530

    Time: 5 ms (59.38%), Space: 10 MB (97.42%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -6,15 +6,15 @@ public:
             if(i==0 && ((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D'))){
                 s.erase(i,2);
             }
-            else if((s[i]=='A' && s[i+1]=='B') || (s[i]=='C' && s[i+1]=='D')){
+            else if(s[i]=='A' && s[i+1]=='B'){
                 s.erase(i,2);
                 i--;
                 
             }
-            // else if(s[i]=='C' && s[i+1]=='D'){
-            //     s.erase(i,2);
-            //     i--;
-            // }
+            else if(s[i]=='C' && s[i+1]=='D'){
+                s.erase(i,2);
+                i--;
+            }
            else i++;
            
         }
