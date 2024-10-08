7a0fe94c56e4905bf657ad2a21836b7997f6791d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-08 21:17:28 +0530

    Time: 106 ms (53.6%), Space: 32.7 MB (54.72%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int minSwaps(string s) {
-       long int open=0,close=0,res=0;
+       int open=0;
         
         for(int i=0; i<s.size(); i++){
             if(s[i]=='[')open++;
             else if(s[i]==']' && open>0)open--;
-            
-            
         }
         
         return (open+1)/2;
