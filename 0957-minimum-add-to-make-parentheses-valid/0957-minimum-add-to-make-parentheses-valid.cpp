c6c0e7feaefe4b1433e79e33caffe3cc881ac2cd
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-08 21:29:36 +0530

    Time: 0 ms (100%), Space: 7.7 MB (94.06%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open=0,close=0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(')open++;
+            else if(open>0 && s[i]==')')open--;
+            else close++;
+        }
+       return open+close;
+    }
+};
