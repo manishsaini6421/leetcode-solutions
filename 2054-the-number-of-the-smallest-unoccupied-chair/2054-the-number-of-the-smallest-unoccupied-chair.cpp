ad17dda416e48752d795cf821d2b6df3945842de
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-11 16:38:55 +0530

    Time: 151 ms (54.64%), Space: 67.6 MB (31.96%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+  
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        vector<pair<pair<int,int>,int>> events;
+        int n=times.size();
+        for(int i=0; i<n; i++){
+            events.push_back({{times[i][0],1},i});
+            events.push_back({{times[i][1],-1},i});
+        }
+        sort(events.begin(),events.end());
+
+        vector<int> assignedChairs(n,-1);
+
+        priority_queue<int,vector<int>,greater<int>> availableChairs;
+
+        for(int i=0; i<n; i++){
+            availableChairs.push(i);
+        }
+
+       
+
+        for(auto event:events){
+            int time=event.first.first;
+            int type=event.first.second;
+            int person=event.second;
+
+            if(person==targetFriend && type==1) return availableChairs.top();
+
+            else if(type==1){
+                assignedChairs[person]=availableChairs.top();
+                availableChairs.pop();
+            }
+            else if(type==-1){
+                availableChairs.push(assignedChairs[person]);
+                assignedChairs[person]=-1;
+            }
+        }
+        return -1;
+        
+    }
+};
