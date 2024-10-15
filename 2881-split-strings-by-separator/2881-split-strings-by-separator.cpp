a37793f9dc61256ef5851ddb046b2e853205c441
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-15 10:23:55 +0530

    Time: 30 ms (36.03%), Space: 43.2 MB (80.16%) - LeetHub

---
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+        int n=words.size();
+        vector<string> res;
+        for(int i=0; i<n; i++){
+            string s="";
+            for(int j=0; j<=words[i].size(); j++){
+                if(words[i][j]==separator && s!="") {
+                    res.push_back(s);
+                    s="";
+                }
+                else if(words[i][j]=='\0' && s!="")res.push_back(s);
+                else if(words[i][j]!=separator) s+=words[i][j];
+            }
+        }
+        return res;
+    }
+};
