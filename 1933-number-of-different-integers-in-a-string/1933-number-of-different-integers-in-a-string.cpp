3cf02f2bb38e6b3c4af34eda8853a33d598dc3ab
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-19 00:28:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (92.06%) - LeetHub

---
+++ 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDifferentIntegers(string word) {
+       unordered_map<int,int> map;
+        long long x=0;
+        int flag=0;
+        for(auto s:word){
+            if(s>='0' && s<='9'){
+                x=x*10+(s-'0');
+                x%=10000000000;
+                flag=1;
+            }
+            else if(flag){
+                map[x]++;
+                cout<<x<<endl;
+                x=0;
+                flag=0;
+            }
+        }
+        if(flag){
+            map[x]++;
+        }
+        
+        
+        return map.size();
+    }
+};
