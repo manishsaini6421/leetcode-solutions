fe8c47d2c9f86b21d38c3ac583ce43e98dc8c59e
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-19 21:26:01 +0530

    Time: 0 ms (100%), Space: 7.4 MB (87.54%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    string flip(string s){
-        int n=s.size();
-        for(int i=0; i<n; i++){
-            if(s[i]=='1')s[i]='0';
-            else s[i]='1';
-        }
-        return s;
-    }
-    string generate(int n){
-        if(n==1) return "0";
-        else if(n==2) return "011";
-        string temp1=generate(n-1);
-        string temp2=flip(temp1);
-        reverse(temp2.begin(),temp2.end());
-        return temp1+"1"+temp2;
-
-    }
+    
     char findKthBit(int n, int k) {
-        string s=generate(n);
-        return s[k-1];
+        if(n==1) return '0';
+        int length =(1<<n)-1;
+        int mid=length/2+1;
+        if(k==mid)return '1';
+        if(k<mid) return findKthBit(n-1,k);
+        return findKthBit(n-1,length-k+1)=='0'?'1':'0';
         
     }
 };
