2ae179c9926f8f5219826d8eee35b9c254503882
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-21 15:07:05 +0530

    Time: 1162 ms (7.89%), Space: 405.5 MB (11.11%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int dfs(int start,string s, unordered_map<string,int>map){
+    int dfs(int start,string s, unordered_set<string>map){
         if(start==s.size()) return 0;
         int maxLength=0;
         for(int i=start+1; i<=s.size(); i++){
             string subs=s.substr(start,i-start);
-            if(!map[subs]){
-                map[subs]++;
+            if(map.find(subs)==map.end()){
+                map.insert(subs);
                 maxLength=max(maxLength,1+dfs(i,s,map));
                 map.erase(subs);
             }
@@ -14,7 +14,7 @@ public:
         return maxLength;
     }
     int maxUniqueSplit(string s) {
-        unordered_map<string,int>map;
+        unordered_set<string>map;
         return dfs(0,s,map);
     }
 };
