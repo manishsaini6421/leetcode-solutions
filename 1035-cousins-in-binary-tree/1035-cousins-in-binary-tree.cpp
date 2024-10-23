cdb2234f8fdb9f24cf853b11f302a54c793e1e16
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-24 00:02:42 +0530

    Time: 0 ms (100%), Space: 12.9 MB (99.48%) - LeetHub

---
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int x, int y,TreeNode* &parent1,TreeNode* &parent2,int &level1,int& level2,int level){
+        if(!root)return;
+        level++;
+        if(root->left && root->left->val==x){
+            parent1=root;
+            level1=level;
+        }
+        if(root->left && root->left->val==y)
+        {
+            parent2=root;
+            level2=level;
+        }
+        if(root->right && root->right->val==x)
+        {
+            parent1=root;
+            level1=level;
+        }
+        if(root->right && root->right->val==y)
+        {
+            parent2=root;
+            level2=level;
+        }
+        if(root->left)dfs(root->left,x, y,parent1,parent2,level1,level2,level);
+       if(root->right) dfs(root->right,x, y,parent1,parent2,level1,level2,level);
+
+    }
+    bool isCousins(TreeNode* root, int x, int y) {
+        TreeNode* parent1;
+        TreeNode* parent2;
+        int level1=0,level2=0;
+        dfs(root,x, y,parent1,parent2,level1,level2,0);
+        // cout<<parent1->val;
+        // cout<<level1<<" "<<level2<<(parent1->val)<<" "<<(parent2->val)<<endl;
+        if(level1==level2 && parent1!=parent2) return true;
+        return false;
+    }
+};
