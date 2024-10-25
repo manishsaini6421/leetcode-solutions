e874d139bcd2fb148965d3c67b01369aa4fb6213
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-25 16:07:37 +0530

    Time: 56 ms (92.86%), Space: 46.5 MB (84.89%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -9,8 +9,8 @@ public:
       int n=folder.size();
       for(int i=1; i<n; i++){
         string temp=v.back();
-        string s=folder[i].substr(0,temp.size());
-        if(temp==s && folder[i][temp.size()]=='/')continue;
+        // string s=folder[i].substr(0,temp.size());
+        if(temp==folder[i].substr(0,temp.size()) && folder[i][temp.size()]=='/')continue;
         else v.push_back(folder[i]);
       }
       return v;
