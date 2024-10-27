7cfac5ce89cae6590888f03920f8c80929413514
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-27 21:28:40 +0530

    Time: 3 ms (81.48%), Space: 29.1 MB (27.15%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -21,7 +21,7 @@ public:
         for(int i=1; i<n; i++){
             for(int j=1; j<m; j++){
                 if(matrix[i][j]==1){
-                    dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
                     ans+=dp[i][j];
                 }
                 
