829e8e3bea65fcf28230b1d33179e6834312e7d8
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-28 23:47:08 +0530

    Time: 87 ms (66.67%), Space: 109.2 MB (50.48%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -13,7 +13,7 @@ public:
             for(int j=1; j<=5; j++){
                 if(map[temp]==1)count++;
                 else break;
-                if(temp>1000)break;
+                if(temp>500)break;
                 temp*=temp;
             }
             if(count>ans)
