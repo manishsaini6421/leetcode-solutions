02aee8a11922eb29843a7e6d82b9f72b1b308fe6
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-29 16:49:36 +0530

    Time: 3 ms (18.25%), Space: 9.2 MB (18.06%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -5,19 +5,16 @@ public:
         vector<int> set={1,2,3,4,5,6,7,8,9};
         for(int i=1; i<=512; i++){
                 vector<int> sub;
+                int sum=0;
                 for(int j=0; j<9; j++){
                     if(1<<j & i)
                     {
                         sub.push_back(set[j]);
+                        sum+=set[j];
                         
                     }
                     
                 }
-                int sum=0;
-                for(auto num:sub){
-                    sum+=num;
-                }
-                
                 if(sum==n && sub.size()==k){
                     subsets.push_back(sub);
                 }
