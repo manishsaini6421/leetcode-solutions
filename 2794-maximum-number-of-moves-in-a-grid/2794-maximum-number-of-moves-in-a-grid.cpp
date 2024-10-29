775b2599e558741a4fe0aff2a1b216f65ed3333e
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-29 11:28:56 +0530

    Time: 25 ms (50%), Space: 74.2 MB (73.46%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -4,36 +4,40 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,0));
-
+        int ans=0;
         for(int i=1; i<n; i++){
             for(int j=0; j<m; j++){
                 if(i==1){
                     if((j>0 && grid[j-1][i-1]<grid[j][i]) || grid[j][i-1]<grid[j][i] || (j<m-1 && grid[j+1][i-1]<grid[j][i] )){
                         dp[j][i]=1;
+                        ans=(dp[j][i]>ans)?dp[j][i]:ans;
                     }
                 }
                 else{
                     if(grid[j][i-1]<grid[j][i] && dp[j][i-1]!=0)
                     {
                         dp[j][i]=1+dp[j][i-1];
+                        ans=(dp[j][i]>ans)?dp[j][i]:ans;
                     } 
                     if(j>0 && grid[j-1][i-1]<grid[j][i] && dp[j-1][i-1]!=0){
                         dp[j][i]=max(1+dp[j-1][i-1],dp[j][i]);
+                        ans=(dp[j][i]>ans)?dp[j][i]:ans;
                     }
                     if(j<m-1 && grid[j+1][i-1]<grid[j][i] &&dp[j+1][i-1]!=0){
                         dp[j][i]=max(1+dp[j+1][i-1],dp[j][i]);
+                        ans=(dp[j][i]>ans)?dp[j][i]:ans;
                     }
                 }    
                 
             }
            
         }
-         int ans=0;
-            for(int j=0; j<m; j++){
-                for(int i=0; i<n; i++){
-                    if(dp[j][i]>ans)ans=dp[j][i];
-                }
-            }
+        //  int ans=0;
+        //     for(int j=0; j<m; j++){
+        //         for(int i=0; i<n; i++){
+        //             if(dp[j][i]>ans)ans=dp[j][i];
+        //         }
+        //     }
             return ans;
     }
 };
