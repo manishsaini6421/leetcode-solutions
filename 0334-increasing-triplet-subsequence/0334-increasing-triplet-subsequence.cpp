3ad399511849ae8e25bf4dfbacea49980d70bcb8
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-10-30 10:47:15 +0530

    Time: 7 ms (15.33%), Space: 114.3 MB (85.45%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         vector<int> v;
+        int count=0;
         for(auto num:nums){
             auto it=lower_bound(v.begin(),v.end(),num);
             if(it==v.end()){
                 v.push_back(num);
+                count++;
+                if(count==3)return true;
             }
             else{
                 *it=num;
             }    
             
         }
-        return (v.size()>=3)?true:false;
+        return false;
     }
 };
