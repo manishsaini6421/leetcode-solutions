3e6c537a6ffaa3fb7f476b791faf0eb834da4ba4
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-11-01 20:51:51 +0530

    Time: 55 ms (69.7%), Space: 67.8 MB (24.54%) - LeetHub

--- 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-    long long fun(vector<int>& robot,vector<int>& factory,int robotIndex,int factIndex,vector<vector<long long>>&dp){
-         if(robotIndex==robot.size())return 0;
-        if(factIndex==factory.size())return 1e12;
-        
-        //If fun(robot,factory,robotIndex,factIndex,dp) already stored indp table then no need to calculate again.
-        if(dp[robotIndex][factIndex]!=-1)return dp[robotIndex][factIndex];
-
-        long long pick=abs(robot[robotIndex]-factory[factIndex])+fun(robot,factory,robotIndex+1,factIndex+1,dp);
-        long long notpick=fun(robot,factory,robotIndex,factIndex+1,dp);
-
-        return dp[robotIndex][factIndex]=min(pick,notpick);
-    }
+    
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
       
         sort(robot.begin(),robot.end());
@@ -20,8 +9,23 @@ public:
        for(auto pair:factory){
         fact.insert(fact.end(),pair[1],pair[0]);
        }
+        int m=robot.size();
+        int n=fact.size();
 
-         vector<vector<long long>> dp(robot.size()+1,vector<long long>(fact.size()+1,-1));
-        return fun(robot,fact,0,0,dp);
+        vector<vector<long long>> dp(m+1,vector<long long>(n+1,0));
+
+        for(int i=0; i<m; i++){
+            dp[i][n]=1e12;
+        }
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                long long take=abs(robot[i]-fact[j])+dp[i+1][j+1];
+                long long dontTake=dp[i][j+1];
+
+                dp[i][j]=min(take,dontTake);
+            }
+        }
+        return dp[0][0];
     }
 };
