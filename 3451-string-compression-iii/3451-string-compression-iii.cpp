c854bc519e4773711ecff4b4ce3990f70f4919c9
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-11-04 14:19:32 +0530

    Time: 1027 ms (5.13%), Space: 27.5 MB (96.77%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        int count=1;
+        string ans="";
+        char temp=word[0];
+        for(int i=1; i<=word.size(); i++){
+            if(temp!=word[i] && count!=9){
+                ans+='0'+count;
+                ans+=temp;
+                
+                cout<<count<<endl;
+                count=1;
+                temp=word[i];
+            }
+            else if(count==9){
+                ans+='0'+count;
+                ans+=temp;
+                
+                cout<<count<<endl;
+                count=1;
+                temp=word[i];
+            } 
+            else count++;
+        }
+        return ans;
+    }
+};
