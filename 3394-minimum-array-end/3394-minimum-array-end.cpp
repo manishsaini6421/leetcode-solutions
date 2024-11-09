940bccb7b1b4c254c7fa03ad8be1d35713503428
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-11-09 13:16:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (12.59%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-       long long int result=x;
-       while(--n){
-        result=(result+1)|x;
-       } 
-       return result;
+        
+       vector<int> bitX(64,0);
+       vector<int> bitN(64,0);
+
+        n--;
+        int i=0;
+        while(x!=0 || n!=0){
+            bitX[i]=x&1;
+            bitN[i]=n&1;
+            x>>=1;
+            n>>=1;
+            i++;
+        }
+        i=0;
+        int j=0;
+        while(i<64){
+            if(bitX[i]==1)i++;
+            else{
+                bitX[i]=bitN[j];
+                i++;
+                j++;
+            }
+            
+        }
+        long long int ans=0;
+        for(int i=0;i<64; i++){
+            ans+=bitX[i]*pow(2,i);
+        }
+        return ans;
     }
 
 };
