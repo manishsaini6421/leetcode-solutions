c8b185f8a9b070d6e307b2f08a7f6e8067b480bd
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-11-10 17:20:37 +0530

    Time: 71 ms (49.45%), Space: 88.3 MB (74.37%) - LeetHub

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        vector<int> v(32, 0);
+        int n = nums.size();
+        int i=0,j = 0;
+        int ans = INT_MAX;
+        int num = 0;
+
+        while (j < n) {
+            int x = nums[j];
+            int index = 0;
+            while (x) {
+                if (x & 1)
+                    v[index]++;
+                index++;
+                x >>= 1;
+            }
+            num = 0;
+            for (int idx = 0; idx < 32; idx++) {
+                if (v[idx]) num += (1 << idx);
+                
+            }
+            while (num >= k && i <= j) {
+                ans = min(ans, j - i + 1);
+                int y = nums[i];
+                int idx = 0;
+                while (y) {
+                    if (y & 1)
+                        v[idx]--;
+                    idx++;
+                    y >>= 1;
+                }
+                i++;
+                num = 0;
+                for (int idx = 0; idx < 32; idx++) {
+                    if (v[idx]) num += (1 << idx);
+                    
+                }
+            }
+            j++;
+        }
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
