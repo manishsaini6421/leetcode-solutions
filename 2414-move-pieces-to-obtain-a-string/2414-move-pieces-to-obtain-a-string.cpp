3e2aa0660a803fbb376b1e43983ea53435ed0a30
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-05 19:09:43 +0530

    Time: 2 ms (95.98%), Space: 21.8 MB (37.84%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -4,28 +4,27 @@ public:
         int i=0,j=0,n=start.size();
         int l=0,r=0;
         while(i<n){
-              if(target[i]=='L'){
-                if(r>0)return false;
-                l++;
-            }
+              
             if(start[i]=='R'){
                 if(l>0)return false;
                 r++;
-            }
-          if(start[i]=='L'){
-                if(l==0)return false;
-                l--;
             }
             if(target[i]=='R'){
                 if(r==0)return false;
                 r--;
             }
-            
+            if(target[i]=='L'){
+                if(r>0)return false;
+                l++;
+            }
+            if(start[i]=='L'){
+                if(l==0)return false;
+                l--;
+            }
             
             i++;
         }
         
-        if(l==0 && r==0)return true;
-        return false;
+        return l==0 && r==0;
     }
 };
