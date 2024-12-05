fa43e0f7ac4408e30402bae31402fd971df6d933
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-05 21:28:43 +0530

    Time: 42 ms (62.72%), Space: 27.4 MB (36.07%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int maximumGain(string s, int x, int y) {
         int result = 0;
+        int n=s.size();
         string temp;
         if(x>y){
             
-            for(int i=0; i<s.size(); i++){
+            for(int i=0; i<n; i++){
                 if(!temp.empty() && temp.back()=='a' && s[i]=='b'){
                     result+=x;
                     temp.pop_back();
@@ -13,7 +14,8 @@ public:
                 else temp.push_back(s[i]);
             }
             string remaining;
-            for(int i=0; i<temp.size(); i++){
+            int t=temp.size();
+            for(int i=0; i<t; i++){
                 if(!remaining.empty() && remaining.back()=='b' && temp[i]=='a'){
                     result+=y;
                     remaining.pop_back();
@@ -22,7 +24,7 @@ public:
             }
         }
         else{
-            for(int i=0; i<s.size(); i++){
+            for(int i=0; i<n; i++){
                 if(!temp.empty() && temp.back()=='b' && s[i]=='a'){
                     result+=y;
                     temp.pop_back();
@@ -30,7 +32,8 @@ public:
                 else temp.push_back(s[i]);
             }
             string remaining;
-            for(int i=0; i<temp.size(); i++){
+            int t=temp.size();
+            for(int i=0; i<t; i++){
                 if(!remaining.empty() && remaining.back()=='a' && temp[i]=='b'){
                     result+=x;
                     remaining.pop_back();
