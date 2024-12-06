bb989e2b17e9aa4f0ea7583fe7c6e51a77107c66
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-06 11:38:45 +0530

    Time: 3 ms (73.08%), Space: 37.5 MB (38.62%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int> result;
         for(int i=0; i<n; i++){
-            nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
+            nums[abs(nums[i])-1]=-1*nums[abs(nums[i])-1];
             if(nums[abs(nums[i])-1]>0)result.push_back(abs(nums[i]));
           
         }
