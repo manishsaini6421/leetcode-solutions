fd5f016d9ce92a80154521ff2440e46f6977f6b0
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-06 12:16:02 +0530

    Time: 0 ms (100%), Space: 12.7 MB (65.14%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-int decimal(string s){
-    int x=0;
-    int n=s.size();
-    for(int i=n-1; i>=0; i--){
-        x+=s[i]*pow(2,n-1-i);
-    }
-    return x;
-}
 string binary(int x,int n){
     string s="";
     for(int i=0; i<n; i++){
@@ -23,14 +15,9 @@ string binary(int x,int n){
     return s;
 }
     string findDifferentBinaryString(vector<string>& nums) {
-        for(int i=0; i<8; i++){
-            cout<<binary(i,3)<<endl;
-        }
-        vector<int> v;
+        
         int n=nums.size();
-        for(int i=0; i<n; i++){
-            v.push_back(decimal(nums[i]));
-        }
+        
         for(int i=0; i<pow(2,n); i++){
             
             if(find(nums.begin(),nums.end(),binary(i,n))==nums.end())return binary(i,n);
