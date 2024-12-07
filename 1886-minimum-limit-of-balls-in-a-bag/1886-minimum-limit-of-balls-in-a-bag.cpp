d954b64d20d78078b17a47c48fa06bdcc5d4033f
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-07 21:37:05 +0530

    Time: 35 ms (54.67%), Space: 59.7 MB (28.46%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -8,7 +8,7 @@ public:
         while(left<right){
             int operations=0;
             int mid=(left+right)/2;
-            for(auto num:nums){
+            for(int num:nums){
                     operations+=(num-1)/mid;
                 
             }
