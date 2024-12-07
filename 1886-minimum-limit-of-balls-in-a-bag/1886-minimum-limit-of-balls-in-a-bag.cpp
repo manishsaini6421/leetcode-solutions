c5396d517df388814c76eec13b9060f15a9c29dc
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-07 21:36:25 +0530

    Time: 31 ms (74%), Space: 59.9 MB (15.54%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -3,20 +3,15 @@ public:
 
     int minimumSize(vector<int>& nums, int maxOperations) {
        int left=1;
-       int right =0;
-       int n=nums.size();
-        for(int i=0; i<n; i++){
-            right=max(right,nums[i]);
-        }
+       int right =*max_element(nums.begin(),nums.end());
         
         int x=0;
         while(left<right){
             int operations=0;
             int mid=(left+right)/2;
-            for(int i=0; i<n; i++){
-                if(nums[i]>mid){
-                    operations+=(nums[i]-1)/mid;
-                } 
+            for(auto num:nums){
+                    operations+=(num-1)/mid;
+                
             }
             if(operations>maxOperations)left=mid+1;
             else right=mid;
