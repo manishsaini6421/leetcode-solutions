4c7625cc48ec88ba2dee6e64aa66f8e61ed1ad67
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-09 00:18:32 +0530

    Time: 158 ms (26.57%), Space: 155.2 MB (22.62%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-int profit(vector<vector<int>>& events,int i,int cnt,vector<vector<int>>& memo,int n){
+int profit(vector<vector<int>>& events,int i,int cnt,vector<vector<int>>& memo){
     if(cnt==2 || i>=events.size())return 0;
     
      if(memo[i][cnt]!=-1) return memo[i][cnt];
-     int nextIndex=upper_bound(events,i,n);
-     int take=events[i][2]+profit(events,nextIndex,cnt+1,memo,n);
-     int donttake=profit(events,i+1,cnt,memo,n);
+     int nextIndex=upper_bound(events,i);
+     int take=events[i][2]+profit(events,nextIndex,cnt+1,memo);
+     int donttake=profit(events,i+1,cnt,memo);
     
     return memo[i][cnt]=max(take,donttake);
 }
 
-int upper_bound(vector<vector<int>>& events, int index,int n){
-    int l=index+1,h=n-1,nextIndex=n;
+int upper_bound(vector<vector<int>>& events, int index){
+    int l=index+1,h=events.size()-1,nextIndex=events.size();
     while(l<=h){
         int mid=(l+h)/2;
         if(events[mid][0]>events[index][1]){
@@ -25,8 +25,8 @@ int upper_bound(vector<vector<int>>& events, int index,int n){
 }
     int maxTwoEvents(vector<vector<int>>& events) {
         sort(events.begin(),events.end());
-        int n=events.size();
-        vector<vector<int>> memo(n,vector<int>(2,-1));
-        return profit(events,0,0,memo,n);
+
+        vector<vector<int>> memo(events.size(),vector<int>(2,-1));
+        return profit(events,0,0,memo);
     }
 };
