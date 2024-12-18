f9e2f61fdf9197b55cc4ad51ba14389341500ace
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2024-12-18 20:41:29 +0530

    Time: 0 ms (100%), Space: 29.5 MB (81.94%) - LeetHub

---
+++ 2159-two-out-of-three/2159-two-out-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        sort(nums3.begin(),nums3.end());
+
+        vector<int> ans;
+        for(int i=1; i<=100; i++){
+            auto it1=find(nums1.begin(),nums1.end(),i);
+            auto it2=find(nums2.begin(),nums2.end(),i);
+            auto it3=find(nums3.begin(),nums3.end(),i);
+            if((it1 != nums1.end() && it2 != nums2.end()) || (it1 != nums1.end() && it3 != nums3.end()) || (it2 != nums2.end() && it3 != nums3.end())) ans.push_back(i);
+        }
+        return ans;
+    }
+};
