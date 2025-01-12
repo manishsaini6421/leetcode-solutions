8335bc1b574d114b06aac11914a72971d273ba50
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-12 13:05:26 +0530

    Time: 20 ms (25.13%), Space: 45.1 MB (8.88%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        if(s.size()%2!=0)return false;
+        vector<long int> v1;
+        vector<long int> v2;
+        for(int i=0; i<s.size(); i++){
+           if(locked[i]=='0')v1.push_back(i);
+           else{
+            if(s[i]=='(')v2.push_back(i);
+            else{
+                if(v2.size()>0)v2.pop_back();
+                else if(v1.size()>0) v1.pop_back();
+                else return false;
+            }
+           }
+        }
+        while(!v2.empty()){
+
+            if(v1.size()>0 && v2[v2.size()-1]<v1[v1.size()-1]){
+                    v1.pop_back();
+                    v2.pop_back();
+                
+            }
+            else return false;
+        }
+        
+        return true;
+    }
+};
