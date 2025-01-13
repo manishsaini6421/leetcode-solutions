14665a5f71b258110b4db5fac7a41baf831a2119
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-13 12:10:20 +0530

    Time: 20 ms (61.29%), Space: 32.1 MB (74.19%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> v(26,0);
+
+        int n=s.size();
+        for(int i=0; i<n; i++){
+            v[s[i]-'a']++;
+        }
+
+        int ans=0;
+
+        for(int i=0; i<26; i++){
+            if(v[i]>0){
+                if(v[i]%2==0)ans+=2;
+            else ans+=1;
+            }
+            
+
+        }
+        return ans;
+    }
+};
