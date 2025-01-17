2f9eae429cce50aa90f0552d0bfe22feb7cd5497
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-17 19:33:36 +0530

    Time: 7 ms (29.44%), Space: 275.8 MB (19.16%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int n=derived.size();
+        vector<int> v(n,0);
+        v[0]=0;
+        for(int i=0; i<n-1; i++){
+            v[i+1]=derived[i]^v[i];
+        }
+        bool x =(v[0]==(derived[n-1]^v[n-1]));
+        v[0]=1;
+        for(int i=0; i<n-1; i++){
+            v[i+1]=derived[i]^v[i];
+        }
+        bool y =(v[0]==(derived[n-1]^v[n-1]));
+
+        return x||y;
+    }
+};
