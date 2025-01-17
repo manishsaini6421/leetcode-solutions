b0ade4328e32ae9da6818afac812676ee83d6dc9
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-18 01:09:53 +0530

    Time: 0 ms (100%), Space: 275.6 MB (23.42%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -4,16 +4,11 @@ public:
         int n=derived.size();
         vector<int> v(n,0);
         v[0]=derived[0]^1;
+        
         for(int i=0; i<n-1; i++){
             v[i+1]=derived[i]^v[i];
         }
-        bool x =(v[0]==(derived[n-1]^v[n-1]));
-        // v[0]=1;
-        // for(int i=0; i<n-1; i++){
-        //     v[i+1]=derived[i]^v[i];
-        // }
-        // bool y =(v[0]==(derived[n-1]^v[n-1]));
 
-        return x;
+        return v[0]==(derived[n-1]^v[n-1]);
     }
 };
