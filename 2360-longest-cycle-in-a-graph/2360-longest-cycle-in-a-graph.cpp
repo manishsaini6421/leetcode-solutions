7ee8cb32018bd7f3182308a745fccd442707628c
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-27 11:15:59 +0530

    Time: 314 ms (18.55%), Space: 212.4 MB (9.62%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int ans=-1;
-    void cycleLength(int node, vector<int>& edges,vector<int> &visited,unordered_map<int,int> &dist){
-        visited[node]=1;
+    void cycleLength(int node, vector<int>& edges,vector<bool> &visited,unordered_map<int,int> &dist){
+        visited[node]=true;
         int neighbor=edges[node];
         if(neighbor!=-1 && !visited[neighbor]){
             dist[neighbor]=dist[node]+1;
@@ -16,7 +16,7 @@ public:
     int longestCycle(vector<int>& edges) {
         int n=edges.size();
         
-        vector<int> visited(n,0);
+        vector<bool> visited(n,0);
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 unordered_map<int,int> dist;
