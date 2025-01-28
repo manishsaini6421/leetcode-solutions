42fb48221b3b24821d28fdf6b05193edf72e0a16
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-28 07:44:19 +0530

    Time: 187 ms (20.91%), Space: 199.3 MB (5%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    static bool custom(vector<int> v1,vector<int> v2){
-        return v1[0]<v2[0];
+    bool bfs(vector<vector<int>> &adj,int node,int target,vector<int> &visited){
+        visited[node]=1;
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int top=q.front();
+
+            q.pop();
+            for(auto val:adj[top]){
+                if(!visited[val]){
+                    if(val==target)return true;
+                    visited[val]=1;
+                    q.push(val);
+                }
+            }
+        }
+        return false;
     }
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites,        vector<vector<int>>& queries) {
+
         vector<vector<int>> adj(numCourses);
-        sort(prerequisites.begin(),prerequisites.end(),custom);
         for(int i=0; i<prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            for(auto num:adj[prerequisites[i][0]]){
-                adj[prerequisites[i][1]].push_back(num);
-            }
+            
         }
 
         vector<bool> ans;
         for(int i=0; i<queries.size(); i++){
-            if(find(adj[queries[i][1]].begin(),adj[queries[i][1]].end(),queries[i][0])!=adj[queries[i][1]].end())ans.push_back(true);
+            vector<int> visited(numCourses,0);
+            if(bfs(adj,queries[i][1],queries[i][0],visited))ans.push_back(true);
             else ans.push_back(false);
         }
         return ans;
