3f34da7ac871635cb455f230f182fd190bb76289
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-01-31 12:49:40 +0530

    Time: 423 ms (22%), Space: 222.7 MB (17%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -7,18 +7,16 @@ public:
         vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int ans = 0;
         int id=0;
-        vector<vector<int>> visited(n, vector<int>(m, 0));
         vector<vector<int>> islandID(n, vector<int>(m, 0));
         unordered_map<int,int> map;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {                
                 int area = 0;
-                if (!visited[i][j] && grid[i][j]) {
+                if (!islandID[i][j] && grid[i][j]) {
                     id++;
                     queue<pair<int, int>> q;
                     q.push({i, j});
-                    visited[i][j] = 1;
                     islandID[i][j]=id;
                     area++;
                     while (!q.empty()) {
@@ -29,10 +27,9 @@ public:
                             int nx = top.first + x;
                             int ny = top.second + y;
                             if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
-                                !visited[nx][ny] && grid[nx][ny]) {
+                                !islandID[nx][ny] && grid[nx][ny]) {
 
                                 q.push({nx, ny});
-                                visited[nx][ny] = 1;
                                 islandID[nx][ny]=id;
                                 area++;
                             }
