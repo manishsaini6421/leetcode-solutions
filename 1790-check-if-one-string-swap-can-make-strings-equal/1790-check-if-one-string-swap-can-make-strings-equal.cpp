a40921b497ca4705eb9932080c13fd2fcf9e758b
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-02-05 14:11:21 +0530

    Time: 2 ms (16.01%), Space: 9 MB (9.35%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        string a1="",a2="";
-        string b1="",b2="";
+        int count=0;
         int n=s1.size();
+        map<char,int> m1,m2;
         for(int i=0; i<n; i++){
-            if(s1[i]!=s2[i]){
-                if(a1==""){
-                     a1=s1[i];
-                    a2=s2[i];
-                }  
-                else if(b1==""){
-                    b1=s1[i];
-                    b2=s2[i];
-                }    
-                else 
-                    return false;
-                                    
-            }
-            
+            if(s1[i]!=s2[i])count++;
+            m1[s1[i]]++;
+            m2[s2[i]]++;
         }
-        if(a1==b2 && a2==b1)
-            return true;
+        for(int i=0; i<26; i++){
+            if(m1['a'+i]!=m2['a'+i])return false;
+        }
+        if(count<=2)return true;
         return false;
     }
 };
