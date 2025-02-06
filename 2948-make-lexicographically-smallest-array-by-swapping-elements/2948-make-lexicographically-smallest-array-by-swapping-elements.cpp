580747c6508eef9746fb7b10cc8d08b11debe16d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-02-06 18:15:16 +0530

    Time: 1582 ms (5%), Space: 218.9 MB (38.11%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -2,23 +2,37 @@ class Solution {
 public:
 
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        int minele=nums[0],maxele=nums[0];
+       
         int n=nums.size();
-        int index=0;
-        for(int i=0; i<n; i++){
-            if(abs(nums[i]-minele)<=limit || abs(nums[i]-maxele)<=limit){
-                maxele=max(nums[i],maxele);
-                minele=min(minele,nums[i]);
+        vector<int> sortedCopy(nums);
+        sort(sortedCopy.begin(),sortedCopy.end());
+       unordered_map<int,int> map;
+       unordered_map<int,vector<int>> List;
+       int group=0;
+       map[sortedCopy[0]]=0;
+        List[0].push_back(sortedCopy[0]);
+        for(int i=1; i<n; i++){
+            if(sortedCopy[i]-sortedCopy[i-1]<=limit){
+                map[sortedCopy[i]]=group;
+                
             }
             else{
+                group++;
+                map[sortedCopy[i]]=group;
                 
-                sort(nums.begin()+index,nums.begin()+i);
-                minele=nums[i];
-                maxele=nums[i];
-                index=i;
             }
+            List[group].push_back(sortedCopy[i]);
         }
-        sort(nums.begin()+index,nums.end());
+        for(auto elements:List){
+            sort(elements.second.begin(),elements.second.end());
+        }
+        for(int i=0; i<n; i++){
+            int group=map[nums[i]];
+            nums[i]=*List[group].begin();
+            List[group].erase(List[group].begin());
+        }
+
+
         return nums;
     }
 };
