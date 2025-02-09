c0b9537c8eb36efb4065996512a567ec5a51e558
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-02-09 11:42:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool manual(pair<int,int> p1,pair<int,int> p2){
+        return p1.second<p2.second;
+    }
+    long long countBadPairs(vector<int>& nums) {
+        long long n=nums.size();
+        long long ans=0;
+          map<int,int> m;
+        for(int i=0; i<n; i++){
+            nums[i]=nums[i]-i;
+            m[nums[i]]++;
+        }
+        long long count=0;
+        for(auto pair:m){
+            int x=pair.second;
+            count+=(x*(x-1))/2;
+        }
+       
+        long long total= n*(n-1)/2;
+        
+        return total - count;
+
+    }
+};
