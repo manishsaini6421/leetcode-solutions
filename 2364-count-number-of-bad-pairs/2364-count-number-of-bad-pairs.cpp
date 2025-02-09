5fda91c5f06946ec946b06b1ab8b2f67e995e4b8
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-02-09 11:52:33 +0530

    Time: 67 ms (67.78%), Space: 87.7 MB (70.06%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    static bool manual(pair<int,int> p1,pair<int,int> p2){
-        return p1.second<p2.second;
-    }
+    
     long long countBadPairs(vector<int>& nums) {
         long long n=nums.size();
           unordered_map<int,int> m;
           long long valid=0;
         for(int i=0; i<n; i++){
-            valid+=m[nums[i]-i];
+            valid+=m[nums[i]-i];//new came difference will pair with all stored in map with same
             m[nums[i]-i]++;
         }
-        
-        
+         
         long long total=n*(n-1)/2;
+
         return total - valid;
 
     }
