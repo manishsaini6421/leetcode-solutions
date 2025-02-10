b00d4d8c7240928e613962d7a3c7c15d7e9d1f2f
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-02-10 16:02:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (53.48%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     string clearDigits(string s) {
         string ans="";
-        int charCount=0;
         int digitCount=0;
         int n=s.size();
         for(int i=n-1; i>=0; i--){
             if(s[i]>='a' && s[i]<='z'){
                 
-                charCount++;
-                if(digitCount<charCount){
+                
+                if(digitCount==0){
                     ans=s[i]+ans;
-                    charCount--;
+                    
                 }
                 else{
-                    charCount--;
                     digitCount--;
                 }
             }
