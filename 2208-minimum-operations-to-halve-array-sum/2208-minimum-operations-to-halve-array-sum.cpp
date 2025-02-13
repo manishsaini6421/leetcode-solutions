874b1f459200c1de808de865c25ee40900eea27b
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-02-13 12:10:08 +0530

    Time: 79 ms (82.28%), Space: 85.4 MB (77.17%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int halveArray(vector<int>& nums) {
         priority_queue<double> maxHeap(nums.begin(),nums.end());
-        long double sum=accumulate(nums.begin(),nums.end(),0.0);
-        long double initialSum=sum;
+        double sum=accumulate(nums.begin(),nums.end(),0.0);
+        double initialSum=sum/2;
         int ans=0;
-        while(sum>initialSum/2){
+        while(sum>initialSum){
             double x=maxHeap.top();
             maxHeap.pop();
             sum-=x;
