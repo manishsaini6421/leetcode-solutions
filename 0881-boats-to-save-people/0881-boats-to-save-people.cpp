ba87c8a7176173f4848610967e9cbacfc444751a
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-01 09:32:08 +0530

    Time: 12 ms (86.48%), Space: 45.7 MB (68.93%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -4,14 +4,18 @@ public:
         sort(people.begin(),people.end());
         int n=people.size();
         int ans=0;
-        for(int i=0; i<n-1; i++){
-            if(people[i]+people[i+1]<=limit){
-                ans++;
-                i++;
-            }
-            else ans++;
+        int i=0,j=n-1;
+       while(i<=j){
+        if(people[i]+people[j]<=limit){
+            ans++;
+            i++;
+            j--;
         }
-        if(people[n-1]+people[n-2]>limit)ans++;
+        else{
+            ans++;
+            j--;
+        }
+       }
         return ans;
     }
 };
