7bc7b10dcdc660eed29a7d5d884e409290baea7f
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-01 17:50:03 +0530

    Time: 25 ms (61.93%), Space: 17.9 MB (68.54%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change-BottomUp_DP.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    
+    int solveTb(vector<int> &coins,int amount){
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<coins.size(); j++){
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX){
+                    dp[i]=min(1+dp[i-coins[j]],dp[i]);
+                }
+            }
+        }
+        return (dp[amount]==INT_MAX)?-1:dp[amount];
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+       
+      
+        return solveTb(coins,amount);
+    }
+};
