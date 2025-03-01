402a91408b991c35b92302e412518f5770bf8a80
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-01 17:57:30 +0530

    Time: 270 ms (5.04%), Space: 65.7 MB (5.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    
-    
-    int solveTb(vector<int> &coins,int amount){
-        vector<int> dp(amount+1,INT_MAX);
-        dp[0]=0;
-        for(int i=1; i<=amount; i++){
-            for(int j=0; j<coins.size(); j++){
-                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX){
-                    dp[i]=min(1+dp[i-coins[j]],dp[i]);
-                }
-            }
+    int solve(vector<int>& coins, int amount, unordered_map<int, int>& dp) {
+        if (amount == 0)
+            return 0;
+
+        if (amount < 0)
+            return INT_MAX;
+
+        if (dp.find(amount) != dp.end())
+            return dp[amount];
+
+        int ans = INT_MAX;
+        for (int i = 0; i < coins.size(); i++) {
+            int temp = solve(coins, amount - coins[i], dp);
+            if (temp != INT_MAX)
+                ans = min(ans, 1 + temp);
         }
-        return (dp[amount]==INT_MAX)?-1:dp[amount];
+        return dp[amount] = ans;
     }
 
     int coinChange(vector<int>& coins, int amount) {
-       
-      
-        return solveTb(coins,amount);
+
+        unordered_map<int, int> dp;
+        int ans = solve(coins, amount, dp);
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
