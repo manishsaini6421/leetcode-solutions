0d5a192576040969fc52534ce296d2168129634b
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-04 13:18:30 +0530

    Time: 0 ms (100%), Space: 8 MB (19.66%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0)return false;
-        int ans=1;
-       for(int i=0; i<=15; i++){
-            cout<<ans<<endl;
-            if(ans==n)return true;
-            if(ans<INT_MAX/4)
-                ans*=4;
-       }
+        int zeroCount=0;
+        int oneCount=0;
+        while(n>0){
+            if(n&1)oneCount++;
+            else zeroCount++;
+            n>>=1;
+        }
+        if(oneCount==1 && zeroCount%2==0)return true;
         return false;
     }
 };
