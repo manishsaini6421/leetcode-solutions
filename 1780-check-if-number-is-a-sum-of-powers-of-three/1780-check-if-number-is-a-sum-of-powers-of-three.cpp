e74a7596b989a2d1f39a3df5fab61354355de4d1
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-04 11:33:03 +0530

    Time: 27 ms (9.16%), Space: 8.1 MB (9.62%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    bool solve(int n,int i,vector<int> &dp){
-        if(n==0)return true;
-        if(n<0 || i<0)return false;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=(solve(n,i-1,dp)  || solve(n-pow(3,i),i-1,dp));
+    bool solve(int n,int i){
+        int x=pow(3,i);
+        if(n==x)return true;
+        if(x>n || n<0)return false;
+        return (solve(n,i+1)  || solve(n-x,i+1));
     }
     bool checkPowersOfThree(int n) {
-        int x=0;
-
-        while(n>=pow(3,x)){
-            x++;
-        }
-        vector<int> dp(n+1,-1);
-        return solve(n,x-1,dp);
+    
+        return solve(n,0);
     }
 };
