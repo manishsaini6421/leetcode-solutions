c61a8a5ab8192bedfb680c3ab16a3b6b457ed51e
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-07 14:47:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        for(int i=2; i<=sqrt(n); i++){
+            if(n%i==0)return false;
+        }
+        return true;
+    }
+    vector<int> closestPrimes(int left, int right) {
+       
+        vector<int> v;
+        for(int i=left; i<=right; i++){
+             if(isPrime(i)){
+                v.push_back(i);
+             }
+        }
+        if(v.size()<=1)return {-1,-1};
+        int first=v[0],second=v[1],diff=v[1]-v[0];
+
+        for(int i=1; i<v.size()-1; i++){
+            if(v[i+1]-v[i]<diff){
+                first=v[i];
+                second=v[i+1];
+                diff=v[i+1]-v[i];
+            }
+        }
+        return {first, second};
+    }
+};
