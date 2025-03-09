1a39e5f5a7eac26dc120b3368d418f5c13971f6d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-09 13:12:21 +0530

    Time: 69 ms (85.06%), Space: 15.4 MB (81.27%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum-2D DP optimized.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int solveTabOpt(vector<int>& nums, int n, int target) {
+        vector<int> prev(target+1,false);
+        vector<int> curr(target+1);
+       
+        for(int j=0; j<=target; j++){
+            prev[j]=(nums[0]==j)?true:false;
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=target; j++){
+                bool take=false;
+                if(j-nums[i]>=0)take=prev[j-nums[i]];
+                bool dontTake=prev[j];
+                curr[j]=(take || dontTake);
+            }
+            prev=curr;
+        }
+       
+        return prev[target];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        
+        if (sum % 2)
+            return false;
+        return solveTabOpt(nums, n , sum / 2);
+    }
+};
