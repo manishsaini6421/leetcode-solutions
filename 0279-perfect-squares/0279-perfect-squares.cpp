5612e1f568a0e5876560a37a4d300b250502ad49
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-09 23:11:31 +0530

    Time: 44 ms (62.03%), Space: 12.9 MB (87.43%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -6,8 +6,9 @@ public:
         dp[0]=0;
         for(int i=1; i<=n; i++){
             for(int j=1; j*j<=i; j++){
-                if(i-j*j>=0)
-                dp[i]=min(dp[i],1+dp[i-j*j]);
+                int temp=j*j;
+                if(i-temp>=0)
+                dp[i]=min(dp[i],1+dp[i-temp]);
             }
         }
         return dp[n];
