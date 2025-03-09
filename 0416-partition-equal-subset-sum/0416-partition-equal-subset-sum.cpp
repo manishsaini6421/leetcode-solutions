5928faf3430fafa24efa3bc112fb1ba7b30ceffe
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-09 13:11:08 +0530

    Time: 75 ms (81.68%), Space: 15.5 MB (79.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     
     int solveTab(vector<int>& nums, int n, int target) {
-        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
-
-        for(int i=0; i<n; i++){
-            dp[i][0]=true;
-        }
+        vector<int> prev(target+1,false);
+        vector<int> curr(target+1);
+       
         for(int j=0; j<=target; j++){
-            dp[0][j]=(nums[0]==j)?true:false;
+            prev[j]=(nums[0]==j)?true:false;
         }
         for(int i=1; i<n; i++){
             for(int j=1; j<=target; j++){
                 bool take=false;
-                if(j-nums[i]>=0)take=dp[i-1][j-nums[i]];
-                bool dontTake=dp[i-1][j];
-                dp[i][j]=(take || dontTake);
+                if(j-nums[i]>=0)take=prev[j-nums[i]];
+                bool dontTake=prev[j];
+                curr[j]=(take || dontTake);
             }
+            prev=curr;
         }
        
-        return dp[n-1][target];
+        return prev[target];
     }
 
     bool canPartition(vector<int>& nums) {
