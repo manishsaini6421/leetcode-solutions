39877c45b83b7377d089be89966f6e13426f3599
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-12 11:57:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int pos=0,neg=0;
+    int searchPos(vector<int>& nums,int i,int j){
+        if(i>j)return -1;
+        int mid=(i+j)/2;
 
-        for(auto num:nums){
-            if(num<0)neg++;
-            else if(num>0)pos++;
+        if(nums[mid]>0){
+            if(mid==0)return 0;
+            else if(nums[mid-1]<=0)return mid;
+            else return searchPos(nums,i,mid-1);
         }
-        return max(pos,neg);
+        return  searchPos(nums,mid+1,j);
+        
+    }
+
+    int searchNeg(vector<int>& nums,int i,int j){
+        if(i>j)return -1;
+        int mid=(i+j)/2;
+
+        if(nums[mid]<0){
+            if(mid==nums.size()-1)return mid;
+            else if(nums[mid+1]>=0)return mid;
+            else return searchNeg(nums,mid+1,j);
+        }
+        return  searchNeg(nums,i,mid-1);
+        
+    }
+
+    int maximumCount(vector<int>& nums) {
+        int n=nums.size();
+        return max(n-searchPos(nums,0,n-1),searchNeg(nums,0,n-1)+1);
+
     }
 };
