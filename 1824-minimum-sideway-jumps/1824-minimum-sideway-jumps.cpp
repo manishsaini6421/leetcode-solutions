2b608b05015ebbc49770cda4f3aa6613daf401a1
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-16 17:16:10 +0530

    Time: 53 ms (70.56%), Space: 271.6 MB (49.27%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -18,9 +18,12 @@ public:
     }
 
     int solveMem(vector<int>& obstacles, int pos, int currLane,vector<vector<int>> &dp) {
-        if (pos == obstacles.size() - 1)
+        int n=obstacles.size() - 1;
+        if (pos == n)
             return 0;
-        if(dp[pos][currLane]!=-1)return dp[pos][currLane];
+        if(dp[currLane][pos]!=-1)
+            return dp[currLane][pos];
+
         if (currLane != obstacles[pos + 1]) {
             return solveMem(obstacles, pos + 1, currLane,dp);
         }
@@ -31,11 +34,14 @@ public:
                 ans=min(ans,1+solveMem(obstacles, pos, newLane,dp));
         }
        
-        return dp[pos][currLane]=ans;
+        return dp[currLane][pos]=ans;
     }
     int minSideJumps(vector<int>& obstacles) { 
         int n=obstacles.size();
-        vector<vector<int>> dp(n,vector<int>(4,-1)); 
-        return solve(obstacles, 0, 2); 
-        }
+       
+        //return solve(obstacles, 0, 2); 
+
+         vector<vector<int>> dp(4,vector<int>(n,-1)); 
+         return solveMem(obstacles,0,2,dp);
+     }
 };
