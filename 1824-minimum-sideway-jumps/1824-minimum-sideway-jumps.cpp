96f8d9233836390b9b70d1bf7027b9dac321d9b9
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-16 16:50:53 +0530

    Time: 374 ms (22.55%), Space: 357.3 MB (15.66%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int>& obstacles, int i, int lane) {
+    int solve(vector<int>& obstacles, int i, int lane,vector<vector<int>> &dp) {
         if (i == obstacles.size() - 1)
             return 0;
-
+        if(dp[i][lane]!=-1)return dp[i][lane];
         if (lane != obstacles[i + 1]) {
-            return solve(obstacles, i + 1, lane);
+            return solve(obstacles, i + 1, lane,dp);
         }
 
         int ans = INT_MAX;
         if (lane == 1) {
             if (obstacles[i] != 2)
-                ans = min(ans, 1 + solve(obstacles, i, 2));
+                ans = min(ans, 1 + solve(obstacles, i, 2,dp));
             if (obstacles[i] != 3)
-                ans = min(ans, 1 + solve(obstacles, i, 3));
+                ans = min(ans, 1 + solve(obstacles, i, 3,dp));
         } else if (lane == 2) {
             if (obstacles[i] != 1)
-                ans = min(ans, 1 + solve(obstacles, i, 1));
+                ans = min(ans, 1 + solve(obstacles, i, 1,dp));
             if (obstacles[i] != 3)
-                ans = min(ans, 1 + solve(obstacles, i, 3));
+                ans = min(ans, 1 + solve(obstacles, i, 3,dp));
         } else if (lane == 3) {
             if (obstacles[i] != 1)
-                ans = min(ans, 1 + solve(obstacles, i, 1));
+                ans = min(ans, 1 + solve(obstacles, i, 1,dp));
             if (obstacles[i] != 2)
-                ans = min(ans, 1 + solve(obstacles, i, 2));
+                ans = min(ans, 1 + solve(obstacles, i, 2,dp));
         }
-        return ans;
+        return dp[i][lane]=ans;
     }
     int minSideJumps(vector<int>& obstacles) { 
-        
-        return solve(obstacles, 0, 2); 
+        int n=obstacles.size();
+        vector<vector<int>> dp(n,vector<int>(4,-1)); 
+        return solve(obstacles, 0, 2,dp); 
         }
 };
