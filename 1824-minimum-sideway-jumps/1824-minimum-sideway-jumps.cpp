6d6582eb535d9f5e882a806a09bdd88ee7d646e5
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-16 17:12:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>& obstacles, int pos, int currLane,vector<vector<int>> &dp) {
-        if (pos == obstacles.size() - 1)
+    int solve(vector<int>& obstacles, int pos, int currLane) {
+        int n=obstacles.size() - 1;
+        if (pos == n)
             return 0;
-        if(dp[pos][currLane]!=-1)return dp[pos][currLane];
         if (currLane != obstacles[pos + 1]) {
-            return solve(obstacles, pos + 1, currLane,dp);
+            return solve(obstacles, pos + 1, currLane);
         }
 
         int ans = INT_MAX;
         for(int newLane=1; newLane<=3; newLane++){
             if(currLane!=newLane && obstacles[pos]!=newLane)
-                ans=min(ans,1+solve(obstacles, pos, newLane,dp));
+                ans=min(ans,1+solve(obstacles, pos, newLane));
+        }
+       
+        return ans;
+    }
+
+    int solveMem(vector<int>& obstacles, int pos, int currLane,vector<vector<int>> &dp) {
+        if (pos == obstacles.size() - 1)
+            return 0;
+        if(dp[pos][currLane]!=-1)return dp[pos][currLane];
+        if (currLane != obstacles[pos + 1]) {
+            return solveMem(obstacles, pos + 1, currLane,dp);
+        }
+
+        int ans = INT_MAX;
+        for(int newLane=1; newLane<=3; newLane++){
+            if(currLane!=newLane && obstacles[pos]!=newLane)
+                ans=min(ans,1+solveMem(obstacles, pos, newLane,dp));
         }
        
         return dp[pos][currLane]=ans;
@@ -19,6 +36,6 @@ public:
     int minSideJumps(vector<int>& obstacles) { 
         int n=obstacles.size();
         vector<vector<int>> dp(n,vector<int>(4,-1)); 
-        return solve(obstacles, 0, 2,dp); 
+        return solve(obstacles, 0, 2); 
         }
 };
