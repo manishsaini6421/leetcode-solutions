deed5d80cf31f409b6ede434e9d99e18b3f5974a
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-16 17:56:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,68 +1,79 @@
 class Solution {
 public:
     int solve(vector<int>& obstacles, int currLane, int pos) {
-        int n=obstacles.size() - 1;
+        int n = obstacles.size() - 1;
         if (pos == n)
             return 0;
         if (currLane != obstacles[pos + 1]) {
-            return solve(obstacles, currLane, pos+1);
+            return solve(obstacles, currLane, pos + 1);
         }
 
         int ans = INT_MAX;
-        for(int newLane=1; newLane<=3; newLane++){
-            if(currLane!=newLane && obstacles[pos]!=newLane)
-                ans=min(ans,1+solve(obstacles, newLane, pos));
+        for (int newLane = 1; newLane <= 3; newLane++) {
+            if (currLane != newLane && obstacles[pos] != newLane)
+                ans = min(ans, 1 + solve(obstacles, newLane, pos));
         }
-       
+
         return ans;
     }
 
-    int solveMem(vector<int>& obstacles, int currLane, int pos,vector<vector<int>> &dp) {
-        int n=obstacles.size() - 1;
+    int solveMem(vector<int>& obstacles, int currLane, int pos,
+                 vector<vector<int>>& dp) {
+        int n = obstacles.size() - 1;
         if (pos == n)
             return 0;
-        if(dp[currLane][pos]!=-1)
+        if (dp[currLane][pos] != -1)
             return dp[currLane][pos];
 
         if (currLane != obstacles[pos + 1]) {
-            return solveMem(obstacles, currLane, pos+1,dp);
+            return solveMem(obstacles, currLane, pos + 1, dp);
         }
 
         int ans = INT_MAX;
-        for(int newLane=1; newLane<=3; newLane++){
-            if(currLane!=newLane && obstacles[pos]!=newLane)
-                ans=min(ans,1+solveMem(obstacles, newLane, pos,dp));
+        for (int newLane = 1; newLane <= 3; newLane++) {
+            if (currLane != newLane && obstacles[pos] != newLane)
+                ans = min(ans, 1 + solveMem(obstacles, newLane, pos, dp));
         }
-       
-        return dp[currLane][pos]=ans;
+
+        return dp[currLane][pos] = ans;
     }
 
-    //   int solveTab(vector<int>& obstacles, int currLane, int pos) {
-    //     int n=obstacles.size() - 1;
-    //     if (pos == n)
-    //         return 0;
-    //     if(dp[currLane][pos]!=-1)
-    //         return dp[currLane][pos];
+    int solveTab(vector<int>& obstacles) {
 
-    //     if (currLane != obstacles[pos + 1]) {
-    //         return solveTab(obstacles, pos + 1, currLane,dp);
-    //     }
+        int n = obstacles.size() - 1;
+        vector<vector<int>> dp(4, vector<int>(obstacles.size(), 1e9));
 
-    //     int ans = INT_MAX;
-    //     for(int newLane=1; newLane<=3; newLane++){
-    //         if(currLane!=newLane && obstacles[pos]!=newLane)
-    //             ans=min(ans,1+solveTab(obstacles, pos, newLane,dp));
-    //     }
-       
-    //     return dp[currLane][pos]=ans;
-    // }
+        dp[0][n] = 0;
+        dp[1][n] = 0;
+        dp[2][n] = 0;
+        dp[3][n] = 0;
 
-    int minSideJumps(vector<int>& obstacles) { 
-        int n=obstacles.size();
-       
-        //return solve(obstacles, 2, 0); 
+        for (int pos = n - 1; pos >= 0; pos--) {
+            for (int currLane = 1; currLane <= 3; currLane++) {
+                if (currLane != obstacles[pos + 1]) {
+                    dp[currLane][pos] = dp[currLane][pos + 1];
+                } else {
+                    int ans = 1e9;
+                    for (int newLane = 1; newLane <= 3; newLane++) {
+                        if (currLane != newLane && obstacles[pos] != newLane)
+                            ans = min(ans, 1 + dp[newLane][pos]);
+                    }
+                    dp[currLane][pos] = ans;
+                }
+            }
+        }
 
-         vector<vector<int>> dp(4,vector<int>(n,-1)); 
-         return solveMem(obstacles,2,0,dp);
-     }
+        return min(dp[2][0], min(1 + dp[1][0], 1 + dp[3][0]));
+    }
+
+    int minSideJumps(vector<int>& obstacles) {
+        int n = obstacles.size();
+
+        // return solve(obstacles, 2, 0);
+
+        // vector<vector<int>> dp(4, vector<int>(n, -1));
+        // return solveMem(obstacles, 2, 0, dp);
+
+        return solveTab(obstacles);
+    }
 };
