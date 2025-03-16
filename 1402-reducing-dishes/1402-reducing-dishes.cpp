1a2a2a76565aab87e45df06a1f41582d6022c938
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-16 21:46:59 +0530

    Time: 9 ms (48.58%), Space: 12.4 MB (56.9%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -54,6 +54,18 @@ public:
         
     }
 
+    int solveOpt_1D(vector<int>& satisfaction){
+          int n=satisfaction.size();
+           vector<int> curr(n+1,0);
+
+           for(int i=n-1; i>=0; i--){
+            for(int time=0; time<=n-1; time++){
+                curr[time]=max((time + 1) * satisfaction[i] + curr[time+1],curr[time]);
+            }
+        }
+        return curr[0];
+    }
+
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end());
 
@@ -66,6 +78,8 @@ public:
 
        //return solveTab(satisfaction);
 
-       return solveOpt(satisfaction);
+       //return solveOpt(satisfaction);
+
+       return solveOpt_1D(satisfaction);
     }
 };
