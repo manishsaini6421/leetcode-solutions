6e6b2e4475b2c90e875ed9c7c3b8613b6b565556
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-18 13:16:25 +0530

    Time: 0 ms (100%), Space: 11.2 MB (79.83%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-    int find(int left,int right){
-        int ans=0;
-        int i=0,j=30;
-        while(i<=j){
-             int mid=i+(j-i)/2;
-            if(pow(2,mid)<right){
-            ans=mid;
-            i=mid+1;
-
-             }
-             else{
-            j=mid-1;
-          }
-        }
-       
-        
-        return (ans<left)?left: pow(2,ans);
-    }
+    
     int rangeBitwiseAnd(int left, int right) {
-        if(left==right)return left;
-        int ans=left;
+       
+        int ans=right;
         
-        for(long i=find(left,right); i<=right; i++){
-            ans&=i;
-            if(!ans)break;
+        while(ans>left){
+            ans=(ans&ans-1);
         }
         return ans;
     }
