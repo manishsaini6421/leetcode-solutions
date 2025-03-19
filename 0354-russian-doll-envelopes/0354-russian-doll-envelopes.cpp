57ccb924726ca1b40179179c90da68b2e25fd740
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-19 14:57:14 +0530

    Time: 3041 ms (5.03%), Space: 489.8 MB (7.28%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    static int manual(vector<int>v1, vector<int>v2){
+        if(v1[0]<v2[0])return v1[0]<v2[0];
+        else if(v1[0]==v2[0]) return v1[1]>v2[1];
+        return 0;
+    }
+    int solve(vector<vector<int>>& envelopes){
+        vector<int> ans;
+        ans.push_back(envelopes[0][1]);
+
+        int n=envelopes.size();
+        for(int i=1; i<n; i++){
+            if(ans.back()<envelopes[i][1]){
+                ans.push_back(envelopes[i][1]);
+            }
+            else{
+                int index=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
+                cout<<index;
+                ans[index]=envelopes[i][1];
+
+            }
+        }
+        return ans.size();
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(),envelopes.end(),manual);
+        int n=envelopes.size();
+
+        for(int i=0; i<n; i++){
+            cout<<envelopes[i][0]<<" "<<envelopes[i][1]<<endl;
+        }
+        return solve(envelopes);
+    }
+};
