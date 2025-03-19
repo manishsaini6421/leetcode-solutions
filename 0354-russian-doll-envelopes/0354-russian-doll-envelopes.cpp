06098b38987997c5cf68e8d09b7fd79aca10a36b
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-19 15:00:13 +0530

    Time: 2107 ms (5.03%), Space: 489.9 MB (6.07%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    static int manual(vector<int>v1, vector<int>v2){
-        if(v1[0]<v2[0])return v1[0]<v2[0];
+    static bool manual(vector<int>v1, vector<int>v2){
+        if(v1[0]<v2[0])return true;
         else if(v1[0]==v2[0]) return v1[1]>v2[1];
-        return 0;
+        return false;
     }
     int solve(vector<vector<int>>& envelopes){
         vector<int> ans;
