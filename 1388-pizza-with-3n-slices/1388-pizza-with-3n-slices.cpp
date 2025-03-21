96b3da06791bb4cb1e961a897468ea3f5f1a85a0
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-21 22:39:35 +0530

    Time: 15 ms (39.04%), Space: 15.4 MB (78.58%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-    int solve(vector<int>& slices,int index,int count){
+    int solve(vector<int>& slices,int index,int endIndex,int count){
         int n=slices.size();
-        if(index>=n || count>n/3)return 0;
+        if(index>endIndex || count==0)return 0;
        
-        int include=slices[index]+solve(slices,index+2,count+1);
+        int include=slices[index]+solve(slices,index+2,endIndex,count-1);
         
-        int exclude=solve(slices,index+1,count);
+        int exclude=solve(slices,index+1,endIndex,count);
         
         return max(include,exclude);
     }
 
-    int solveMem(vector<int>& slices,int index,int count, vector<vector<int>> &dp){
+    int solveMem(vector<int>& slices,int index,int endIndex,int count, vector<vector<int>> &dp){
         int n=slices.size();
-        if(index>=n || count==0)return 0;
+        if(index>endIndex || count==0)return 0;
 
         if(dp[index][count]!=-1)return dp[index][count];
 
-        int include=slices[index]+solveMem(slices,index+2,count-1,dp);
+        int include=slices[index]+solveMem(slices,index+2,endIndex,count-1,dp);
         
-        int exclude=solveMem(slices,index+1,count,dp);
+        int exclude=solveMem(slices,index+1,endIndex,count,dp);
         
         return dp[index][count]=max(include,exclude);
     }
 
     int maxSizeSlices(vector<int>& slices) {
         int n=slices.size();
-        vector<int> v1;
-        vector<int> v2;
-        for(int i=0; i<n; i++){
-            if(i!=n-1){
-                v1.push_back(slices[i]);
-            }
-            if(i!=0){
-                v2.push_back(slices[i]);
-            }
-        }
       
-        //return max(solve(v1,0,0),solve(v2,0,0));
+        //return max(solve(slices,0,n-2,n/3),solve(slices,1,n-1,n/3));
 
-         vector<vector<int>> dp1(n,vector<int> (n/3+1,-1));
-         int case1=solveMem(v1,0,n/3,dp1);
-         vector<vector<int>> dp2(n,vector<int> (n/3+1,-1));
-         int case2=solveMem(v2,0,n/3,dp2);
+         vector<vector<int>> dp(n,vector<int> (n/3+1,-1));
+         int case1=solveMem(slices,0,n-2,n/3,dp);
+         
+         for(int i=0; i<n; i++){
+            for(int j=0; j<=n/3; j++){
+                dp[i][j]=-1;
+            }
+         }
+         int case2=solveMem(slices,1,n-1,n/3,dp);
          return max(case1,case2);
     }
 };
