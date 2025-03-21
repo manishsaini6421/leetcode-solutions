3e8238ea36d5857aa85ba9d7a369d9441c90a72e
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-21 23:10:59 +0530

    Time: 3 ms (96.45%), Space: 12 MB (97.85%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -52,6 +52,45 @@ public:
 
         return max(dp1[0][n/3],dp2[1][n/3]);
     }
+
+    int solveOpt(vector<int>& slices){
+        int n=slices.size();
+        vector<int> next_12(n/3+1,0);
+        vector<int> next_11(n/3+1,0);
+        vector<int> curr_1(n/3+1,0);
+
+        
+
+        for(int index=n-2; index>=0; index--){
+            for(int count=1; count<=n/3; count++){
+                int include=slices[index]+next_12[count-1];
+        
+                int exclude=next_11[count];
+        
+                curr_1[count]=max(include,exclude);
+            }
+            next_12=next_11;
+            next_11=curr_1;
+        }
+
+         vector<int> next_22(n/3+1,0);
+        vector<int> next_21(n/3+1,0);
+        vector<int> curr_2(n/3+1,0);
+
+        for(int index=n-1; index>=1; index--){
+            for(int count=1; count<=n/3; count++){
+                int include=slices[index]+next_22[count-1];
+        
+                int exclude=next_21[count];
+        
+                curr_2[count]=max(include,exclude);
+            }
+             next_22=next_21;
+            next_21=curr_2;
+        }
+
+        return max(curr_1[n/3],next_21[n/3]);
+    }
     int maxSizeSlices(vector<int>& slices) {
         int n=slices.size();
       
@@ -64,6 +103,8 @@ public:
         //  int case2=solveMem(slices,1,n-1,n/3,dp2);
         //  return max(case1,case2);
 
-        return solveTab(slices);
+        //return solveTab(slices);
+
+        return solveOpt(slices);
     }
 };
