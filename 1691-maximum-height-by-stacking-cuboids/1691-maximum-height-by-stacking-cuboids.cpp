0289f7040cd74dff6df1942505132bb24d89edfd
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-21 15:30:59 +0530

    Time: 1823 ms (5.88%), Space: 12.6 MB (93.77%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -16,14 +16,33 @@ public:
 
         return max(include,exclude);
     }
+
+    int solveIndex(vector<vector<int>>& cuboids, int curr, int prev) {
+        if (curr == cuboids.size())
+            return 0;
+
+        int w = cuboids[curr][0], l = cuboids[curr][1],h = cuboids[curr][2];
+        
+        int include=0;
+
+        if(prev==-1 || w>=cuboids[prev][0] && l>=cuboids[prev][1] && h>=cuboids[prev][2]){
+            include=h+solveIndex(cuboids,curr+1,curr);
+        }
+        int exclude=solveIndex(cuboids,curr+1,prev);
+
+        return max(include,exclude);
+    }
+
     int maxHeight(vector<vector<int>>& cuboids) {
         for(auto &cuboid:cuboids){
             sort(cuboid.begin(),cuboid.end());
         }
         sort(cuboids.begin(),cuboids.end());
 
-        vector<int> v={0,0,0};
-        return solve(cuboids,0,v);
+        // vector<int> v={0,0,0};
+        // return solve(cuboids,0,v);
+
+        return solveIndex(cuboids,0,-1);
           
     }
 };
