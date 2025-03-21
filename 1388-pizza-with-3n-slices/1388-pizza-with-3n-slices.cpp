371251c8a30ae03cfe2a4b029d1c188b06912b18
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-21 23:05:48 +0530

    Time: 8 ms (76.55%), Space: 18.3 MB (64.89%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -23,17 +23,47 @@ public:
         
         return dp[index][count]=max(include,exclude);
     }
+    
+    int solveTab(vector<int>& slices){
+        int n=slices.size();
+        vector<vector<int>> dp1(n+2,vector<int> (n/3+1,0));
 
+        for(int index=n-2; index>=0; index--){
+            for(int count=1; count<=n/3; count++){
+                int include=slices[index]+dp1[index+2][count-1];
+        
+                int exclude=dp1[index+1][count];
+        
+                dp1[index][count]=max(include,exclude);
+            }
+        }
+
+        vector<vector<int>> dp2(n+2,vector<int> (n/3+1,0));
+
+        for(int index=n-1; index>=1; index--){
+            for(int count=1; count<=n/3; count++){
+                int include=slices[index]+dp2[index+2][count-1];
+        
+                int exclude=dp2[index+1][count];
+        
+                dp2[index][count]=max(include,exclude);
+            }
+        }
+
+        return max(dp1[0][n/3],dp2[1][n/3]);
+    }
     int maxSizeSlices(vector<int>& slices) {
         int n=slices.size();
       
         //return max(solve(slices,0,n-2,n/3),solve(slices,1,n-1,n/3));
 
-         vector<vector<int>> dp1(n,vector<int> (n/3+1,-1));
-         int case1=solveMem(slices,0,n-2,n/3,dp1);
+        //  vector<vector<int>> dp1(n,vector<int> (n/3+1,-1));
+        //  int case1=solveMem(slices,0,n-2,n/3,dp1);
          
-         vector<vector<int>> dp2(n,vector<int> (n/3+1,-1));
-         int case2=solveMem(slices,1,n-1,n/3,dp2);
-         return max(case1,case2);
+        //  vector<vector<int>> dp2(n,vector<int> (n/3+1,-1));
+        //  int case2=solveMem(slices,1,n-1,n/3,dp2);
+        //  return max(case1,case2);
+
+        return solveTab(slices);
     }
 };
