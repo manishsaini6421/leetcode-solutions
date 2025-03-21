7fe96d921efa5fc15eeb76b99d6a70a8d7c09f5d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-21 22:57:13 +0530

    Time: 13 ms (48.8%), Space: 18.2 MB (71.1%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -29,15 +29,11 @@ public:
       
         //return max(solve(slices,0,n-2,n/3),solve(slices,1,n-1,n/3));
 
-         vector<vector<int>> dp(n,vector<int> (n/3+1,-1));
-         int case1=solveMem(slices,0,n-2,n/3,dp);
+         vector<vector<int>> dp1(n,vector<int> (n/3+1,-1));
+         int case1=solveMem(slices,0,n-2,n/3,dp1);
          
-         for(int i=0; i<n; i++){
-            for(int j=0; j<=n/3; j++){
-                dp[i][j]=-1;
-            }
-         }
-         int case2=solveMem(slices,1,n-1,n/3,dp);
+         vector<vector<int>> dp2(n,vector<int> (n/3+1,-1));
+         int case2=solveMem(slices,1,n-1,n/3,dp2);
          return max(case1,case2);
     }
 };
