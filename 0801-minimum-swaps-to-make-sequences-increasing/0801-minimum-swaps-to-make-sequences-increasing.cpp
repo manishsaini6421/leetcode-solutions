32e9e316ae5240815de281771db771a73685ea63
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-23 17:40:11 +0530

    Time: 119 ms (7.39%), Space: 138.6 MB (20.04%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -4,59 +4,87 @@ public:
         if (index == nums1.size())
             return 0;
 
-        int prev1=nums1[index-1];
-        int prev2=nums2[index-1];
+        int prev1 = nums1[index - 1];
+        int prev2 = nums2[index - 1];
 
-        int ans=INT_MAX;
-        
-        if(swaped)
-            swap(prev1,prev2);
+        int ans = INT_MAX;
 
-        if(prev1<nums1[index] && prev2<nums2[index])
-            ans=min(ans,solve(nums1,nums2,index+1,0));
+        if (swaped)
+            swap(prev1, prev2);
 
-        if(prev1<nums2[index] && prev2<nums1[index])
-            ans=min(ans,1+solve(nums1,nums2,index+1,1));        
-        
+        if (prev1 < nums1[index] && prev2 < nums2[index])
+            ans = solve(nums1, nums2, index + 1, 0);
+
+        if (prev1 < nums2[index] && prev2 < nums1[index])
+            ans = min(ans, 1 + solve(nums1, nums2, index + 1, 1));
+
         return ans;
     }
 
-
-    int solveMem(vector<int>& nums1, vector<int>& nums2, int index, bool swaped,vector<vector<int>> &dp) {
+    int solveMem(vector<int>& nums1, vector<int>& nums2, int index, bool swaped,
+                 vector<vector<int>>& dp) {
         if (index == nums1.size())
             return 0;
 
-        if(dp[index][swaped]!=-1){
+        if (dp[index][swaped] != -1) {
             return dp[index][swaped];
         }
 
-        int prev1=nums1[index-1];
-        int prev2=nums2[index-1];
+        int prev1 = nums1[index - 1];
+        int prev2 = nums2[index - 1];
 
-        int ans=INT_MAX;
-        
-        if(swaped)
-            swap(prev1,prev2);
+        int ans = INT_MAX;
 
-        if(prev1<nums1[index] && prev2<nums2[index])
-            ans=min(ans,solveMem(nums1,nums2,index+1,0,dp));
+        if (swaped)
+            swap(prev1, prev2);
 
-        if(prev1<nums2[index] && prev2<nums1[index])
-            ans=min(ans,1+solveMem(nums1,nums2,index+1,1,dp));        
-        
-        return dp[index][swaped]=ans;
+        if (prev1 < nums1[index] && prev2 < nums2[index])
+            ans = solveMem(nums1, nums2, index + 1, 0, dp);
+
+        if (prev1 < nums2[index] && prev2 < nums1[index])
+            ans = min(ans, 1 + solveMem(nums1, nums2, index + 1, 1, dp));
+
+        return dp[index][swaped] = ans;
     }
 
+    int solveTab(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int index = n - 1; index >= 1; index--) {
+            for (int swaped = 1; swaped >= 0; swaped--) {
+                int prev1 = nums1[index - 1];
+                int prev2 = nums2[index - 1];
+
+                int ans = INT_MAX;
+
+                if (swaped)
+                    swap(prev1, prev2);
+
+                if (prev1 < nums1[index] && prev2 < nums2[index])
+                    ans = min(ans, dp[index + 1][0]);
+
+                if (prev1 < nums2[index] && prev2 < nums1[index])
+                    ans = min(ans, 1 + dp[index + 1][1]);
+
+                dp[index][swaped] = ans;
+            }
+        }
+
+        return dp[n - 1][0];
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
 
         bool swaped = 0;
 
-        //return solve(nums1, nums2, 1, swaped);
+        // return solve(nums1, nums2, 1, swaped);
 
-        int n=nums1.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solveMem(nums1,nums2,1,0,dp);
+        int n = nums1.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solveMem(nums1, nums2, 1, 0, dp);
+
+        //return solveTab(nums1, nums2);
     }
 };
