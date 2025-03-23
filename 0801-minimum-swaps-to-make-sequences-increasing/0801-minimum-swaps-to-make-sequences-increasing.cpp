d1570e0e71aea3def5172fa81b503c26e87067c7
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-23 17:18:05 +0530

    Time: 111 ms (17.08%), Space: 138.4 MB (25.58%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int solve(vector<int>& nums1, vector<int>& nums2, int index, bool swaped) {
+        if (index == nums1.size())
+            return 0;
+
+        int prev1=nums1[index-1];
+        int prev2=nums2[index-1];
+
+        int ans=INT_MAX;
+        
+        if(swaped)
+            swap(prev1,prev2);
+
+        if(prev1<nums1[index] && prev2<nums2[index])
+            ans=min(ans,solve(nums1,nums2,index+1,0));
+
+        if(prev1<nums2[index] && prev2<nums1[index])
+            ans=min(ans,1+solve(nums1,nums2,index+1,1));        
+        
+        return ans;
+    }
+
+
+    int solveMem(vector<int>& nums1, vector<int>& nums2, int index, bool swaped,vector<vector<int>> &dp) {
+        if (index == nums1.size())
+            return 0;
+
+        if(dp[index][swaped]!=-1){
+            return dp[index][swaped];
+        }
+
+        int prev1=nums1[index-1];
+        int prev2=nums2[index-1];
+
+        int ans=INT_MAX;
+        
+        if(swaped)
+            swap(prev1,prev2);
+
+        if(prev1<nums1[index] && prev2<nums2[index])
+            ans=min(ans,solveMem(nums1,nums2,index+1,0,dp));
+
+        if(prev1<nums2[index] && prev2<nums1[index])
+            ans=min(ans,1+solveMem(nums1,nums2,index+1,1,dp));        
+        
+        return dp[index][swaped]=ans;
+    }
+
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+
+        bool swaped = 0;
+
+        //return solve(nums1, nums2, 1, swaped);
+
+        int n=nums1.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solveMem(nums1,nums2,1,0,dp);
+    }
+};
