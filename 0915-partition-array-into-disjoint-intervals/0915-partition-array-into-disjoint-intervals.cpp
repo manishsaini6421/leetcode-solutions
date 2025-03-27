14b03db335b25be5b9feb01333cac27c25c2313a
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-27 21:32:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> right(n,INT_MAX);
+        right[n-1]=nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            right[i]=min(right[i+1],nums[i]);
+        }
+        int left=INT_MIN;
+        for(int i=0; i<n-1; i++){
+            left=max(left,nums[i]);
+            cout<<left<<" "<<right[i+1]<<endl;
+            if(left<right[i+1])return i+1;
+        }
+        return n;
+    }
+};
