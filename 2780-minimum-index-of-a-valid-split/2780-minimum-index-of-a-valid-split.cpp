315f77e7a20f3727504b213bf8843c2f625b442a
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-27 21:21:04 +0530

    Time: 4 ms (77.46%), Space: 90 MB (64.34%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -12,24 +12,16 @@ public:
             }
         }
         count=0;
-        vector<int> v(n,0);
-
-        if(nums[0]==num){
-            v[0]=1;
-            count++;
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]==num)count++;
         }
 
-        for(int i=1; i<n; i++){
-            if(nums[i]==num){
-                v[i]=1;
-                count++;
-            }
-                
-            v[i]+=v[i-1];
-        }
+        int freq=0;
 
         for(int i=0; i<n; i++){
-            if(v[i]>(i+1)/2 && count-v[i]>(n-i-1)/2)return i;
+            if(nums[i]==num)freq++;
+            if(freq>(i+1)/2 && count-freq>(n-i-1)/2)return i;
         }
         return -1;
     }
