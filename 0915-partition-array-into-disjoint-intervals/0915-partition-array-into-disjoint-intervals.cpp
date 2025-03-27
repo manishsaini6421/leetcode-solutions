08739ddac50cc52502f0ee1575f70ad40772e53f
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-27 22:02:23 +0530

    Time: 8 ms (25.93%), Space: 102.1 MB (54.58%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -10,7 +10,6 @@ public:
         int left=INT_MIN;
         for(int i=0; i<n-1; i++){
             left=max(left,nums[i]);
-            cout<<left<<" "<<right[i+1]<<endl;
             if(left<=right[i+1])return i+1;
         }
         return n;
