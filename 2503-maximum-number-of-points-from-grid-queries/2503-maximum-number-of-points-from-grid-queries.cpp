a7fc306cde07f2ed9504eddb4008ce9858e6bf49
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-03-28 23:48:36 +0530

    Time: 1648 ms (5.07%), Space: 42 MB (59.06%) - LeetHub

--- 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
+++ 2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& grid, vector<pair<int, int>>& mapping, vector<int>& ans) {
+    void bfs(vector<vector<int>>& grid, vector<pair<int, int>>& mapping,
+             vector<int>& ans) {
+
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            q;
+
         int m = grid.size();
         int n = grid[0].size();
-        
+
         vector<vector<int>> visited(m, vector<int>(n, 0));
+
         vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        // Min-heap (priority queue) to process cells in increasing order of value
-        priority_queue<pair<int, pair<int, int>>, 
-                       vector<pair<int, pair<int, int>>>, 
-                       greater<pair<int, pair<int, int>>>> q;
-        
-        // Start BFS from the top-left corner
+        int index = 0;
+
         q.push({grid[0][0], {0, 0}});
         visited[0][0] = 1;
 
-        int index = 0, count = 0;
+        int count = 0;
 
-        while (!q.empty() && index < mapping.size()) {
-            int value = q.top().first;
-            int x = q.top().second.first;
-            int y = q.top().second.second;
-            q.pop();
+        for (auto [val, i] : mapping) {
+            while (!q.empty() && q.top().first < val) {
+                auto top = q.top();
+                q.pop();
 
-            // If the grid value is less than the current query threshold
-            while (index < mapping.size() && value >= mapping[index].first) {
-                ans[mapping[index].second] = count;
-                index++;
-            }
-            count++;
+                count++;
+                if (index < ans.size())
+                    cout << mapping[index].first << " "
+                         << ans[mapping[index].second] << endl;
 
-            // Explore all 4 directions
-            for (auto [dx, dy] : directions) {
-                int nx = x + dx;
-                int ny = y + dy;
+                for (auto dir : directions) {
+                    int x = top.second.first + dir.first;
+                    int y = top.second.second + dir.second;
 
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
-                    q.push({grid[nx][ny], {nx, ny}});
-                    visited[nx][ny] = 1;
+                    if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]) {
+                        q.push({grid[x][y], {x, y}});
+                        visited[x][y] = 1;
+                    }
                 }
             }
-        }
-
-        // Fill remaining answers (queries that couldn't be satisfied)
-        while (index < mapping.size()) {
-            ans[mapping[index].second] = count;
-            index++;
+            ans[i]=count;
         }
     }
-
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
         int k = queries.size();
         vector<pair<int, int>> mapping;
@@ -59,6 +54,10 @@ public:
         }
         sort(mapping.begin(), mapping.end());
 
+        for (auto pair : mapping) {
+            cout << pair.first << " " << pair.second << endl;
+        }
+        cout << endl;
         vector<int> ans(k, 0);
         bfs(grid, mapping, ans);
         return ans;
