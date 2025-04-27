e297f9e1e0f7c6f899b1dba3e42f6b8480947ec7
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-04-27 10:15:19 +0530

    Time: 503 ms (5.04%), Space: 45.6 MB (6.83%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        map<int,int> freq;
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            freq[nums[i]]++;
+        }
+        int ans=0;
+
+        int key=freq.begin()->first;
+        int value=freq.begin()->second;
+        for(auto pair:freq){
+            cout<<pair.first<<" "<<pair.second<<endl;
+            if(pair.first-key==1 && pair.second+value>ans){
+                ans=pair.second+value;
+            }
+            key=pair.first;
+            value=pair.second;
+        }
+        return ans;
+    }
+};
