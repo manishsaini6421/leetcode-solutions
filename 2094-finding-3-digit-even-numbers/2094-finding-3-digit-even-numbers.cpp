8ebdf61b58d6d39e6bf02db34afe9290b91c2e3c
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-12 09:30:28 +0530

    Time: 238 ms (25.26%), Space: 13.7 MB (56.14%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,66 +1,30 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
+        unordered_set<int> nums;  // Target even set
         int n = digits.size();
-        vector<int> ans;
-        set<int> temp;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (digits[i] != 0) {
-                        int num =
-                            stoi(to_string(digits[i]) + to_string(digits[j]) +
-                                 to_string(digits[k]));
-                        
-                        if (num % 2 == 0) {
-                            temp.insert(num);
-                        }
-                        num =
-                            stoi(to_string(digits[i]) + to_string(digits[k]) +
-                                 to_string(digits[j]));
-                        
-                        if (num % 2 == 0) {
-                            temp.insert(num);
-                        }
+        // Traverse the indices of three digits
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    // Determine whether it meets the condition of the target
+                    // even number
+                    if (i == j || j == k || i == k) {
+                        continue;
                     }
-                    if (digits[j] != 0) {
-                        int num =
-                            stoi(to_string(digits[j]) + to_string(digits[i]) +
-                                 to_string(digits[k]));
-                        
-                        if (num % 2 == 0) {
-                            temp.insert(num);
-                        }
-                        num =
-                            stoi(to_string(digits[j]) + to_string(digits[k]) +
-                                 to_string(digits[i]));
-                        
-                        if (num % 2 == 0) {
-                            temp.insert(num);
-                        }
-                    }
-                    if (digits[k] != 0) {
-                        int num =
-                            stoi(to_string(digits[k]) + to_string(digits[j]) +
-                                 to_string(digits[i]));
-                        
-                        if (num % 2 == 0) {
-                            temp.insert(num);
-                        }
-                        num =
-                            stoi(to_string(digits[k]) + to_string(digits[i]) +
-                                 to_string(digits[j]));
-                        
-                        if (num % 2 == 0) {
-                            temp.insert(num);
-                        }
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if (num >= 100 && num % 2 == 0) {
+                        nums.insert(num);
                     }
                 }
             }
         }
-        for (auto num : temp) {
-            ans.push_back(num);
+        // Converted to an array sorted in ascending order
+        vector<int> res;
+        for (const int num : nums) {
+            res.push_back(num);
         }
-        return ans;
+        sort(res.begin(), res.end());
+        return res;
     }
 };
