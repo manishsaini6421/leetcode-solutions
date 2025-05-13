2b6da9fdcc6b8f3c2655b558183044c0658bcc0f
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-13 10:42:46 +0530

    Time: 403 ms (56.65%), Space: 19.5 MB (67.98%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -2,36 +2,28 @@
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        vector<int> arr(26,0);
-        vector<int> brr(26,0);
+        vector<int> arr(26, 0);
+        vector<int> brr(26, 0);
         int n = s.size();
 
         for (int i = 0; i < n; i++) {
             arr[s[i] - 'a']++;
         }
-        for(int i=0; i<26; i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
+        
         for (int i = 0; i < t; i++) {
             fill(brr.begin(), brr.end(), 0);
 
             for (int j = 0; j < 26; j++) {
 
                 if (j == 25) {
-                    brr[0] = (brr[0]+arr[25])%MOD;
-                    brr[1] = (brr[1]+arr[25])%MOD;
+                    brr[0] = (brr[0] + arr[25]) % MOD;
+                    brr[1] = (brr[1] + arr[25]) % MOD;
                 } else {
-                    brr[j + 1] = (brr[j+1]+arr[j])%MOD;
+                    brr[j + 1] = (brr[j + 1] + arr[j]) % MOD;
                 }
-               
             }
-            arr=brr;
-            for(int j=0; j<26; j++){
-            cout<<arr[j]<<" ";
-        }
-        cout<<endl;
-            
+            arr = brr;
+           
         }
         int ans = 0;
         for (int j = 0; j < 26; j++) {
