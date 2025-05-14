37dc315beebb6066c78a0b69bca33a1828d31419
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-14 13:41:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -11,8 +11,28 @@ public:
         }
         return dp[n]=ans;
     }
+    int solveTab(int n){
+        if(n==0)return 1;
+        if(n==1 || n==2)return n;
+        if(n==3)return 5;
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int j=4; j<=n; j++){
+            int ans=0;
+            for(int i=0; i<n; i++){
+                ans+=dp[i]*dp[j-i-1];
+            }
+            dp[j]=ans;
+        }
+        return dp[n];
+    }
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solve(n,dp);
+
+        return solveTab(n);
     }
 };
