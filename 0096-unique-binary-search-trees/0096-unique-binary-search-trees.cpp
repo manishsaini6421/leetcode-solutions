709f56033c8555e14fbd806d500a149c1a6196a8
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-14 13:43:23 +0530

    Time: 0 ms (100%), Space: 8 MB (64.82%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int solve(int n,vector<int> &dp){
-        if(n==0)return 1;
-        if(n==1 || n==2)return n;
-        if(n==3)return 5;
+        if(n<=1)return 1;
         if(dp[n]!=-1)return dp[n];
         int ans=0;
         for(int i=0; i<n; i++){
@@ -30,9 +28,9 @@ public:
         return dp[n];
     }
     int numTrees(int n) {
-        // vector<int> dp(n+1,-1);
-        // return solve(n,dp);
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
 
-        return solveTab(n);
+        // return solveTab(n);
     }
 };
