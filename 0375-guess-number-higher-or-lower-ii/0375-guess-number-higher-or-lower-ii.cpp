a8a90fb20b743e7124737841e8a334fc3c77bc08
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-14 15:04:40 +0530

    Time: 83 ms (35.95%), Space: 11.1 MB (52.87%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -9,8 +9,21 @@ public:
        
         return ans;
     }
+    int solveMem(int i,int j,vector<vector<int>> &dp){
+        if(i>=j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=INT_MAX;
+        for(int k=i; k<=j; k++){
+            ans=min(ans,k+max(solveMem(i,k-1,dp),solveMem(k+1,j,dp)));
+        }
+       
+        return dp[i][j]=ans;
+    }
     int getMoneyAmount(int n) {
        
-                return solve(1,n);
+        // return solve(1,n);
+
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solveMem(1,n,dp);
     }
 };
