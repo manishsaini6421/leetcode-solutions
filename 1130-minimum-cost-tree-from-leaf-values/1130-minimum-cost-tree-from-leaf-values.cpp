b73f6eb0267a20393a03103aeb7f80615e52a6e5
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-14 18:09:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    int solve(int i,int j,map<pair<int,int>,int> &maxi){
+        if(i>=j)return 0;
+        int ans=INT_MAX;
+        for(int k=i; k<j; k++){
+            ans=min(ans,maxi[{i,k}]*maxi[{k+1,j}]+solve(i,k,maxi)+solve(k+1,j,maxi));
+        }
+        return ans;
+    }
+    int mctFromLeafValues(vector<int>& arr) {
+       map<pair<int,int>,int> maxi;
+       int n=arr.size();
+       for(int i=0; i<n; i++){
+            maxi[{i,i}]=arr[i];
+            for(int j=i+1; j<n; j++){
+                maxi[{i,j}]=max(arr[j],maxi[{i,j-1}]);
+            }
+       }
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                cout<<i<<"->"<<j<<"ans is ->"<<maxi[{i,j}]<<endl;
+            }
+        }
+        return solve(0,n-1,maxi);
+    }
+};
