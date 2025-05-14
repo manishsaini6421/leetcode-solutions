5a1063927a462e5376e18cc396e9cfc528f94a03
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-14 12:54:55 +0530

    Time: 122 ms (20%), Space: 81.4 MB (42.05%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int destroyTargets(vector<int>& nums, int space) {
-        sort(nums.begin(),nums.end());
-       map<int,int> dp;
-       for(auto num:nums){
-        cout<<num<<" "<<num%space<<endl;
-        dp[num%space]++;
-       }
-       int ans=0;
-       int temp=0;
-      for(auto num:nums){
-       if(temp< dp[num%space]){
-        temp=dp[num%space];
-        ans=num;
-       }
-      }
+        sort(nums.begin(), nums.end());
+        map<int, int> dp;
+        for (auto num : nums) {
+            dp[num % space]++;
+        }
+        int ans = 0;
+        int temp = 0;
+        for (auto num : nums) {
+            if (temp < dp[num % space]) {
+                temp = dp[num % space];
+                ans = num;
+            }
+        }
         return ans;
-        
-
     }
 };
