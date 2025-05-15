ddff6f76f6a9ea75f70d50a245251cef06bc7def
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-15 12:12:20 +0530

    Time: 0 ms (100%), Space: 17.2 MB (63.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -53,6 +53,26 @@ public:
         return dp[0][1];
     }
 
+    int solveTabOpt(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> next(2,0),curr(2,0);
+        for (int index = n - 1; index >= 0; index--) {
+            for (int flag = 0; flag <= 1; flag++) {
+                int buy = 0;
+                if (flag) {
+                    buy = next[0]- prices[index];
+                }
+                int sell = 0;
+                if (!flag) {
+                    sell = prices[index] + next[1];
+                }
+                int skip = next[flag];
+                curr[flag] = max(skip, max(buy, sell));
+            }
+            next=curr;
+        }
+        return curr[1];
+    }
     int maxProfit(vector<int>& prices) {
         // return solve(0, 1, prices);
 
@@ -60,6 +80,8 @@ public:
         // vector<vector<int>> dp(n + 1, vector<int>(2, -1));
         // return solveMem(0, 1, prices, dp);
 
-        return solveTab(prices);
+        // return solveTab(prices);
+
+        return solveTabOpt(prices);
     }
 };
