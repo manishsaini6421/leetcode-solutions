8f6f73e0de80c1fb713ddfff9c75903919a5ecb1
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-15 16:59:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+    int solve(int index,int flag,int limit,vector<int>& prices){
+        if(index==prices.size())return 0;
+        if(limit==0)return 0;
+        int buy=0;
+        int sell=0;
+        if(flag){
+            buy=solve(index+1,0,limit,prices)-prices[index];
+        }
+        
+        else{
+            sell=prices[index]+solve(index+1,1,limit-1,prices);
+        }
+        int skip=solve(index+1,flag,limit,prices);
+
+        return max(skip,max(buy,sell));
+    }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<int> profit(n+1,0);
-        int maxi=prices[n-1];
-
-        for(int i=n-1; i>=0; i--){
-            profit[i]=max(profit[i+1],maxi-prices[i]);
-            maxi=max(maxi,prices[i]);
-
-        }
-       
-        int mini=prices[0];
-        int ans=0;
-        for(int i=0; i<n; i++){
-            ans=max(ans,prices[i]-mini+profit[i+1]);
-            mini=min(mini,prices[i]);
-        }
-        return ans;
+        return solve(0,1,2,prices);
 
     }
 };
