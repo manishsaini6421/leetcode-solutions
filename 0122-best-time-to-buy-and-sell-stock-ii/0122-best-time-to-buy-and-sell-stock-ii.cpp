6ee14331d3c3a7be91749d8841c0adc6828659ea
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-15 12:00:15 +0530

    Time: 11 ms (5.93%), Space: 21.9 MB (15.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -15,14 +15,29 @@ public:
         return max(skip, max(buy, sell));
     }
 
+    int solveMem(int index, int flag, vector<int>& prices,vector<vector<int>> &dp) {
+        if (index >= prices.size())
+            return 0;
+        if(dp[index][flag]!=-1)return dp[index][flag];    
+        int buy = 0;
+        if (flag) {
+            buy = solveMem(index + 1, 0, prices,dp)-prices[index];
+        }
+        int sell = 0;
+        if (!flag) {
+            sell = prices[index] +  solveMem(index + 1, 1, prices,dp);
+        }
+        int skip = solveMem(index + 1, flag, prices,dp);
+        return dp[index][flag]=max(skip, max(buy, sell));
+    }
    
 
    int maxProfit(vector<int>& prices) {
-        return solve(0, 1, prices);
+        //return solve(0, 1, prices);
 
-        // int n = prices.size();
-        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        // return solveMem(0, -1, prices, dp);
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        return solveMem(0, 1, prices, dp);
 
         //return solveTab(prices);
     }
