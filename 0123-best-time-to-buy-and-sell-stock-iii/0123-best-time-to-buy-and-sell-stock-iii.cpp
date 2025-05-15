f33f20bdc72ff0bb04534a07458727dcd1a1bd40
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-15 17:14:46 +0530

    Time: 404 ms (39.16%), Space: 211.4 MB (42.09%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,49 +1,87 @@
 class Solution {
 public:
-    int solve(int index,int flag,int limit,vector<int>& prices){
-        if(index==prices.size())return 0;
-        if(limit==0)return 0;
-        int buy=0;
-        int sell=0;
-        if(flag){
-            buy=solve(index+1,0,limit,prices)-prices[index];
+    int solve(int index, int flag, int limit, vector<int>& prices) {
+        if (index == prices.size())
+            return 0;
+        if (limit == 0)
+            return 0;
+        int buy = 0;
+        int sell = 0;
+        if (flag) {
+            buy = solve(index + 1, 0, limit, prices) - prices[index];
         }
-        
-        else{
-            sell=prices[index]+solve(index+1,1,limit-1,prices);
-        }
-        int skip=solve(index+1,flag,limit,prices);
 
-        return max(skip,max(buy,sell));
+        else {
+            sell = prices[index] + solve(index + 1, 1, limit - 1, prices);
+        }
+        int skip = solve(index + 1, flag, limit, prices);
+
+        return max(skip, max(buy, sell));
     }
 
-    int solveMem(int index,int flag,int limit,vector<int>& prices,  vector<vector<vector<int>>> &dp){
-        if(index==prices.size())return 0;
-        if(limit==0)return 0;
+    int solveMem(int index, int flag, int limit, vector<int>& prices,
+                 vector<vector<vector<int>>>& dp) {
+        if (index == prices.size())
+            return 0;
+        if (limit == 0)
+            return 0;
 
-        if(dp[index][flag][limit]!=-1)return dp[index][flag][limit];
-        int buy=0;
-        int sell=0;
+        if (dp[index][flag][limit] != -1)
+            return dp[index][flag][limit];
+        int buy = 0;
+        int sell = 0;
 
-        if(flag){
-            buy=solveMem(index+1,0,limit,prices,dp)-prices[index];
+        if (flag) {
+            buy = solveMem(index + 1, 0, limit, prices, dp) - prices[index];
         }
-        
-        else{
-            sell=prices[index]+solveMem(index+1,1,limit-1,prices,dp);
-        }
-        int skip=solveMem(index+1,flag,limit,prices,dp);
 
-        return dp[index][flag][limit]=max(skip,max(buy,sell));
+        else {
+            sell =
+                prices[index] + solveMem(index + 1, 1, limit - 1, prices, dp);
+        }
+        int skip = solveMem(index + 1, flag, limit, prices, dp);
+
+        return dp[index][flag][limit] = max(skip, max(buy, sell));
+    }
+    int solveTab(vector<int>& prices) {
+        int n = prices.size();
+        int Limit = 2;
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(Limit + 1, 0)));
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int flag = 0; flag <= 1; flag++) {
+                for (int limit = 1; limit <= Limit; limit++) {
+                    int buy = 0;
+                    int sell = 0;
+
+                    if (flag) {
+                        buy = dp[index + 1][0][limit] -
+                              prices[index];
+                    }
+
+                    else {
+                        sell = prices[index] +
+                               dp[index + 1][1][limit - 1];
+                    }
+                    int skip = dp[index + 1][flag][limit];
+
+                    dp[index][flag][limit] = max(skip, max(buy, sell));
+                }
+            }
+        }
+        return dp[0][1][Limit];
     }
     int maxProfit(vector<int>& prices) {
-       // return solve(0,1,2,prices);
+        // return solve(0,1,2,prices);
 
-        int n=prices.size();
-        int limit=2;
-       vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2,vector<int> (limit+1,-1)));
+        // int n = prices.size();
+        // int limit = 2;
+        // vector<vector<vector<int>>> dp(
+        //     n + 1, vector<vector<int>>(2, vector<int>(limit + 1, -1)));
 
-       return solveMem(0,1,2,prices,dp);
+        // return solveMem(0, 1, 2, prices, dp);
 
+        return solveTab(prices);
     }
 };
