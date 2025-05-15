abdcd8bc9c965dc1cbca9934076e579533ab4679
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-15 09:05:55 +0530

    Time: 10 ms (55.44%), Space: 10.9 MB (94.74%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n=s.size();
-        int ans=0,j=0,k=0;
-        int arr[3]={0};
-        for(int i=0; i<n; i++){
-            arr[s[i]-'a']++;
-            if(arr[0] && arr[1] && arr[2]){
-                while(arr[0] && arr[1] && arr[2]){
-                    arr[s[j]-'a']--;
-                    j++;
-                }
-                ans+=(n-i+j-k-1 + (j-k-1)*(n-i-1));
-                k=j;
+        int n = s.size();
+        int ans = 0;
+        int count[3] = {0}; // count of 'a', 'b', and 'c'
+        int left = 0;
+
+        for (int right = 0; right < n; ++right) {
+            count[s[right] - 'a']++;
+
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                count[s[left] - 'a']--;
+                left++;
             }
-           
-            
+
+            // All substrings from [left-1..right] to [0..right] are valid
+            ans += left;
         }
         return ans;
     }
