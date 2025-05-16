28b803bae9c2a658baaf9ec89a4db491dbf2172d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 09:35:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int hamming(string& s1, string& s2) {
+        int count = 0;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i])
+                count++;
+        }
+        return count;
+    }
+    vector<string> getWordsInLongestSubsequence(vector<string>& words,
+                                                vector<int>& groups) {
+        int n = words.size();
+
+        vector<int> dp(n + 1, 1);
+        vector<int> parent(n, -1);
+
+        int index=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (groups[j] != groups[i] &&
+                    words[j].size() == words[i].size() &&
+                    hamming(words[j], words[i]) == 1) {
+                        if(dp[j]+1>dp[i]){
+                            dp[i]=dp[j]+1;
+                            parent[i]=j;
+                            index=i;
+                        }
+                }
+            }
+        }
+        vector<string> ans;
+        while(index!=-1){
+            ans.push_back(words[index]);
+            index=parent[index];
+        }
+        reverse(ans.begin(),ans.end());
+        
+
+        return ans;
+    }
+};
