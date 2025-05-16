c842131a5eff15097f55b7ef828c14cd71dfc6b5
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 15:05:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,40 +1,61 @@
 class Solution {
 public:
-    int solve(string &s,int i,int j){
-        if(i==j)return 1;
-        if(i>j)return 0;
+    int solve(string& s, int i, int j) {
+        if (i == j)
+            return 1;
+        if (i > j)
+            return 0;
 
-        int ans=0;
-        if(s[i]==s[j]){
-            ans=2+solve(s,i+1,j-1);
-        }
-        else{
-            ans=max(solve(s,i+1,j),solve(s,i,j-1));
+        int ans = 0;
+        if (s[i] == s[j]) {
+            ans = 2 + solve(s, i + 1, j - 1);
+        } else {
+            ans = max(solve(s, i + 1, j), solve(s, i, j - 1));
         }
         return ans;
     }
 
-    int solveMem(string &s,int i,int j, vector<vector<int>> &dp){
-        if(i==j)return 1;
-        if(i>j)return 0;
+    int solveMem(string& s, int i, int j, vector<vector<int>>& dp) {
+        if (i == j)
+            return 1;
+        if (i > j)
+            return 0;
 
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ans=0;
-        if(s[i]==s[j]){
-            ans=2+solveMem(s,i+1,j-1,dp);
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int ans = 0;
+        if (s[i] == s[j]) {
+            ans = 2 + solveMem(s, i + 1, j - 1, dp);
+        } else {
+            ans = max(solveMem(s, i + 1, j, dp), solveMem(s, i, j - 1, dp));
         }
-        else{
-            ans=max(solveMem(s,i+1,j,dp),solveMem(s,i,j-1,dp));
+        return dp[i][j] = ans;
+    }
+
+    int solveTab(string& s) {
+        int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                int ans = 0;
+                if (s[i] == s[j]) {
+                    ans = 2 + solveMem(s, i + 1, j - 1, dp);
+                } else {
+                    ans = max(solveMem(s, i + 1, j, dp),
+                              solveMem(s, i, j - 1, dp));
+                }
+                dp[i][j] = ans;
+            }
         }
-        return dp[i][j]=ans;
+        return dp[0][n-1];
     }
     int longestPalindromeSubseq(string s) {
         // return solve(s,0,s.size()-1);
 
-        int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        return solveMem(s,0,n-1,dp);
+        // int n = s.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        // return solveMem(s, 0, n - 1, dp);
 
-
+        return solveTab(s);
     }
 };
