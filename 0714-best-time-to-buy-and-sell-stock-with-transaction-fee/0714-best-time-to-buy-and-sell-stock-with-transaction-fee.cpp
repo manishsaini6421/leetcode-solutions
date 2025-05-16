9757a32f4585966c747f124bd93561c7669c597d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 12:22:54 +0530

    Time: 101 ms (38.38%), Space: 95 MB (49.5%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,47 +1,72 @@
 class Solution {
 public:
-    int solve(int index, int flag,vector<int>& prices,int fee){
-        if(index==prices.size())return 0;
+    int solve(int index, int flag, vector<int>& prices, int fee) {
+        if (index == prices.size())
+            return 0;
 
-        int buy=0;
-        int sell=0;
-        if(flag){
-            buy=solve(index+1,0,prices,fee)-prices[index]-fee;
+        int buy = 0;
+        int sell = 0;
+        if (flag) {
+            buy = solve(index + 1, 0, prices, fee) - prices[index] - fee;
+        } else {
+            sell = prices[index] + solve(index + 1, 1, prices, fee);
         }
-        else{
-            sell=prices[index]+solve(index+1,1,prices,fee);
-        }
 
-        int skip=solve(index+1,flag,prices,fee);
+        int skip = solve(index + 1, flag, prices, fee);
 
-        return max(skip,max(buy,sell));
-
+        return max(skip, max(buy, sell));
     }
 
-     int solveMem(int index, int flag,vector<int>& prices,int fee,vector<vector<int>> &dp){
-        if(index==prices.size())return 0;
+    int solveMem(int index, int flag, vector<int>& prices, int fee,
+                 vector<vector<int>>& dp) {
+        if (index == prices.size())
+            return 0;
 
-        if(dp[index][flag]!=-1)return dp[index][flag];
-        int buy=0;
-        int sell=0;
-        if(flag){
-            buy=solveMem(index+1,0,prices,fee,dp)-prices[index]-fee;
+        if (dp[index][flag] != -1)
+            return dp[index][flag];
+        int buy = 0;
+        int sell = 0;
+        if (flag) {
+            buy = solveMem(index + 1, 0, prices, fee, dp) - prices[index] - fee;
+        } else {
+            sell = prices[index] + solveMem(index + 1, 1, prices, fee, dp);
         }
-        else{
-            sell=prices[index]+solveMem(index+1,1,prices,fee,dp);
+
+        int skip = solveMem(index + 1, flag, prices, fee, dp);
+
+        return dp[index][flag] = max(skip, max(buy, sell));
+    }
+
+    int solveTab(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int flag = 0; flag <= 1; flag++) {
+                int buy = 0;
+                int sell = 0;
+                if (flag) {
+                    buy = dp[index + 1][0] -
+                          prices[index] - fee;
+                } else {
+                    sell =
+                        prices[index] + dp[index + 1][1];
+                }
+
+                int skip = dp[index + 1][flag];
+
+                dp[index][flag] = max(skip, max(buy, sell));
+            }
         }
-
-        int skip=solveMem(index+1,flag,prices,fee,dp);
-
-        return dp[index][flag]=max(skip,max(buy,sell));
-
+        return dp[0][1];
     }
     int maxProfit(vector<int>& prices, int fee) {
         // return solve(0,1,prices,fee);
 
-        int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int> (2,-1));
-        return solveMem(0,1,prices,fee,dp);
+        // int n = prices.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        // return solveMem(0, 1, prices, fee, dp);
 
+        return solveTab(prices,fee);
     }
 };
