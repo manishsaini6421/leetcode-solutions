b4f57e1ad99f7c9b7af686079e9f66aa76f91e07
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 12:24:55 +0530

    Time: 22 ms (58.6%), Space: 59 MB (67.56%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -60,6 +60,32 @@ public:
         }
         return dp[0][1];
     }
+
+    int solveTabOpt(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<int> next(2,0);
+        vector<int> curr(2,0);
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int flag = 0; flag <= 1; flag++) {
+                int buy = 0;
+                int sell = 0;
+                if (flag) {
+                    buy = next[0] -
+                          prices[index] - fee;
+                } else {
+                    sell =
+                        prices[index] + next[1];
+                }
+
+                int skip = next[flag];
+
+                curr[flag] = max(skip, max(buy, sell));
+            }
+            next=curr;
+        }
+        return curr[1];
+    }
     int maxProfit(vector<int>& prices, int fee) {
         // return solve(0,1,prices,fee);
 
@@ -67,6 +93,8 @@ public:
         // vector<vector<int>> dp(n + 1, vector<int>(2, -1));
         // return solveMem(0, 1, prices, fee, dp);
 
-        return solveTab(prices,fee);
+        // return solveTab(prices,fee);
+
+        return solveTabOpt(prices,fee);
     }
 };
