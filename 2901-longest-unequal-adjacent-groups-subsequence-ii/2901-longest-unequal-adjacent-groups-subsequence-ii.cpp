e9ac3770a0cf1e0a5538cb2c591c4510ece1d7eb
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 09:38:14 +0530

    Time: 90 ms (37.5%), Space: 34.5 MB (59.38%) - LeetHub

--- 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -15,7 +15,8 @@ public:
         vector<int> dp(n + 1, 1);
         vector<int> parent(n, -1);
 
-        int index=0;
+        int longestSub=1;
+        int longestSubIdx=0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
@@ -25,15 +26,18 @@ public:
                         if(dp[j]+1>dp[i]){
                             dp[i]=dp[j]+1;
                             parent[i]=j;
-                            index=i;
+                            if(longestSub<dp[i]){
+                                longestSub=dp[i];
+                                longestSubIdx=i;
+                            }
                         }
                 }
             }
         }
         vector<string> ans;
-        while(index!=-1){
-            ans.push_back(words[index]);
-            index=parent[index];
+        while(longestSubIdx!=-1){
+            ans.push_back(words[longestSubIdx]);
+            longestSubIdx=parent[longestSubIdx];
         }
         reverse(ans.begin(),ans.end());
         
