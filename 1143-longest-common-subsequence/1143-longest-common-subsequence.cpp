9b02202a146e1e1a4b0e8a0f07006210543a90d0
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 13:13:55 +0530

    Time: 30 ms (36.82%), Space: 27.2 MB (76.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,40 +1,65 @@
 class Solution {
 public:
-    int solve(int i,int j, string &text1, string &text2){
-        if(i==text1.size() || j==text2.size())return 0;
-        int length=0;
-        if(text1[i]==text2[j]){
-            length=1+solve(i+1,j+1,text1,text2);
-        }
-        else{
-            length=max(solve(i+1,j,text1,text2),solve(i,j+1,text1,text2));
+    int solve(int i, int j, string& text1, string& text2) {
+        if (i == text1.size() || j == text2.size())
+            return 0;
+        int length = 0;
+        if (text1[i] == text2[j]) {
+            length = 1 + solve(i + 1, j + 1, text1, text2);
+        } else {
+            length = max(solve(i + 1, j, text1, text2),
+                         solve(i, j + 1, text1, text2));
         }
         return length;
     }
 
-    int solveMem(int i,int j, string &text1, string &text2,vector<vector<int>> &dp){
-        if(i==text1.size() || j==text2.size())return 0;
+    int solveMem(int i, int j, string& text1, string& text2,
+                 vector<vector<int>>& dp) {
+        if (i == text1.size() || j == text2.size())
+            return 0;
 
-        if(dp[i][j]!=-1)return dp[i][j];
-        int length=0;
-        if(text1[i]==text2[j]){
-            length=1+solveMem(i+1,j+1,text1,text2,dp);
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int length = 0;
+        if (text1[i] == text2[j]) {
+            length = 1 + solveMem(i + 1, j + 1, text1, text2, dp);
+        } else {
+            length = max(solveMem(i + 1, j, text1, text2, dp),
+                         solveMem(i, j + 1, text1, text2, dp));
         }
-        else{
-            length=max(solveMem(i+1,j,text1,text2,dp),solveMem(i,j+1,text1,text2,dp));
+        return dp[i][j] = length;
+    }
+
+    int solveTab(string& text1, string& text2) {
+        int m = text1.size();
+        int n = text2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n+1, 0));
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int length = 0;
+                if (text1[i] == text2[j]) {
+                    length = 1 + dp[i + 1][j + 1];
+                } else {
+                    length = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+                dp[i][j] = length;
+            }
         }
-        return dp[i][j]=length;
+        return dp[0][0];
     }
     int longestCommonSubsequence(string text1, string text2) {
-    
-        //return solve(0,0,text1,text2);
 
-        int m=text1.size();
-        int n=text2.size();
+        // return solve(0,0,text1,text2);
 
-        vector<vector<int>> dp(m+1,vector<int> (n,-1));
+        // int m = text1.size();
+        // int n = text2.size();
 
-        return solveMem(0,0,text1,text2,dp);
-        
+        // vector<vector<int>> dp(m + 1, vector<int>(n, -1));
+
+        // return solveMem(0, 0, text1, text2, dp);
+
+        return solveTab(text1, text2);
     }
 };
