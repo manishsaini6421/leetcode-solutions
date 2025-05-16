28a61eb1469d44c0c8f6d751f1b46d01d4800d88
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 12:20:51 +0530

    Time: 119 ms (17.03%), Space: 100.3 MB (19.63%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -18,27 +18,30 @@ public:
 
     }
 
-     int solveMem(int index, int transactionNo,vector<int>& prices,int fee){
+     int solveMem(int index, int flag,vector<int>& prices,int fee,vector<vector<int>> &dp){
         if(index==prices.size())return 0;
 
+        if(dp[index][flag]!=-1)return dp[index][flag];
         int buy=0;
         int sell=0;
-        if(transactionNo%2==0){
-            buy=solveMem(index+1,transactionNo+1,prices,fee)-prices[index]-fee;
+        if(flag){
+            buy=solveMem(index+1,0,prices,fee,dp)-prices[index]-fee;
         }
         else{
-            sell=prices[index]+solveMem(index+1,transactionNo+1,prices,fee);
+            sell=prices[index]+solveMem(index+1,1,prices,fee,dp);
         }
 
-        int skip=solveMem(index+1,transactionNo,prices,fee);
+        int skip=solveMem(index+1,flag,prices,fee,dp);
 
-        return max(skip,max(buy,sell));
+        return dp[index][flag]=max(skip,max(buy,sell));
 
     }
     int maxProfit(vector<int>& prices, int fee) {
-        return solve(0,1,prices,fee);
+        // return solve(0,1,prices,fee);
 
         int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int> (2,-1));
+        return solveMem(0,1,prices,fee,dp);
 
     }
 };
