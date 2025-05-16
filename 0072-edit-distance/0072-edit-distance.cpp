125244353bcdc33fbfa5ee99ecd2e8c731f29929
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-16 15:55:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int solve(int i, int j, string& word1, string& word2) {
+
+        if (i == word1.size())
+            return word2.size() - j;
+        if (j == word2.size())
+            return word1.size() - i;
+
+        int ans = 0;
+        if (word1[i] == word2[j]) {
+            ans = solve(i + 1, j + 1, word1, word2);
+        } else {
+            ans = 1 + min(solve(i + 1, j + 1, word1, word2),
+                          min(solve(i + 1, j, word1, word2),
+                              solve(i, j + 1, word1, word2)));
+        }
+        return ans;
+    }
+
+    int solveMem(int i, int j, string& word1, string& word2,vector<vector<int>> &dp) {
+
+        if (i == word1.size())
+            return word2.size() - j;
+        if (j == word2.size())
+            return word1.size() - i;
+
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        int ans = 0;
+        if (word1[i] == word2[j]) {
+            ans = solveMem(i + 1, j + 1, word1, word2,dp);
+        } else {
+            ans = 1 + min(solveMem(i + 1, j + 1, word1, word2,dp),
+                          min(solveMem(i + 1, j, word1, word2,dp),
+                              solveMem(i, j + 1, word1, word2,dp)));
+        }
+        return dp[i][j]=ans;
+    }
+    int minDistance(string word1, string word2) {
+        return solve(0, 0, word1, word2);
+
+        int m=word1.size();
+        int n=word2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+
+        return solveMem(0,0,word1,word2,dp);
+    }
+};
