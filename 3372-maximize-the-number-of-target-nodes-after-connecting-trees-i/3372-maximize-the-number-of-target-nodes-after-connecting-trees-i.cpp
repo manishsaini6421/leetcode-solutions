291e7392ff4946dbcf0e64598a2cbeb2a71f2610
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-29 08:04:57 +0530

    Time: 727 ms (7.4%), Space: 217 MB (19.91%) - LeetHub

--- 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void bfs(map<int, set<int>>& adj,vector<int> &dp,int i,int k){
+    int bfs(map<int, set<int>>& adj,int i,int k){
         int n=adj.size();
         vector<int> visited(n,0);
         int count=0;
@@ -26,7 +26,7 @@ public:
             }
             level++;
         }
-        dp[i]=count;
+        return count;
     }
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
         map<int ,set<int>> adj1;
@@ -37,7 +37,7 @@ public:
         int n=adj1.size();
         vector<int> dp1(n,0);
         for(int i=0; i<n; i++){
-            bfs(adj1,dp1,i,k);
+            dp1[i]=bfs(adj1,i,k);
         }
          map<int ,set<int>> adj2;
         for(auto edge:edges2){
@@ -45,17 +45,14 @@ public:
             adj2[edge[1]].insert(edge[0]);
         }
         int m=adj2.size();
-        vector<int> dp2(m,0);
         int maxVal=0;
         for(int i=0; i<m; i++){
-            bfs(adj2,dp2,i,k-1);
-            maxVal=max(maxVal,dp2[i]);
+            
+            maxVal=max(maxVal,bfs(adj2,i,k-1));
         }
         
-        vector<int> ans;
         for(int i=0; i<n; i++){
             dp1[i]+=maxVal;
-            cout<<dp1[i]<<" ";
         }
         
         return dp1;
