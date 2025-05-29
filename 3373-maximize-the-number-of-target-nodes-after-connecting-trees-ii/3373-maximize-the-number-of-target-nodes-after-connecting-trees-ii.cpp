2564271cc5518245f8add358cda4286baf591dc6
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-29 22:53:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -32,6 +32,7 @@ public:
     }
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
         unordered_map<int,unordered_set<int>> adj1,adj2;
+        
         for(auto &edge:edges1){
             adj1[edge[0]].insert(edge[1]);
              adj1[edge[1]].insert(edge[0]);
@@ -41,15 +42,19 @@ public:
             adj2[edge[0]].insert(edge[1]);
              adj2[edge[1]].insert(edge[0]);
         }
-        vector<int> levels1(adj1.size(),0);
-        vector<int> levels2(adj2.size(),0);
+
+        int n=adj1.size();
+        int m=adj2.size();
+
+        vector<int> levels1(n,0);
+        vector<int> levels2(n,0);
 
         auto tree1=BFS(adj1,levels1);
         auto tree2=BFS(adj2,levels2);
 
         vector<int> ans;
 
-        for(int i=0; i<adj1.size(); i++){
+        for(int i=0; i<n; i++){
             int temp=max(tree2.first,tree2.second);
             ans.push_back(levels1[i]%2?(tree1.second + temp):(tree1.first+temp));
         }
