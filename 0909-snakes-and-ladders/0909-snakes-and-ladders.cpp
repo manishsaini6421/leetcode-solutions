181203ff62ccae65283e5572488363a259d9a268
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-05-31 19:54:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    pair<int,int> getCoordinates(int n,int v){
+        int row=n-1-(v-1)/n;
+        int col=(n-row)%2==1?(v-1)%n:n-(v-1)%n-1;
+
+        return {row,col};
+    }
+    int BFS(vector<vector<int>>& board){
+        int n=board.size();
+        vector<bool> visited(n*n+1,false);
+
+        queue<int> q;
+        q.push(1);
+        visited[1]=true;
+        int level=0;
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0; i<size; i++){
+                int top=q.front();
+                if(top==n*n)return level;
+                q.pop();
+                for(int i=1; i<=6; i++){
+                    int newVal=min(n*n,top+i);
+                    
+                    if(!visited[newVal]){
+                        visited[newVal]=true;
+                        q.push(newVal);
+                    }
+                    pair<int,int> coordinate=getCoordinates(n,newVal);
+                    int row=coordinate.first;
+                    int col=coordinate.second;
+                    if(board[row][col]!=-1){
+                        q.push(board[row][col]);
+                        visited[board[row][col]]=true;
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+    int snakesAndLadders(vector<vector<int>>& board) {
+
+        
+        return BFS(board);
+    }
+};
