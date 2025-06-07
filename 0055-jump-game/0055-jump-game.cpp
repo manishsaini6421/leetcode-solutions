ba7604fc13cb6706d85c27b0f4e56a3a25dffd44
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-06-07 09:22:31 +0530

    Time: 0 ms (100%), Space: 52.4 MB (33.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -39,14 +39,14 @@ public:
     bool solveGreedy(vector<int>& nums){
         int maxReach=0;
         int n=nums.size();
-        for(int i=0; i<n && i<maxReach; i++){
+        for(int i=0; i<n && i<=maxReach; i++){
             maxReach=max(maxReach,nums[i]+i);
             if(maxReach>=n-1)return true;
         }
         return false;
     }
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return solveTab(nums);
+        // vector<int> dp(nums.size(), -1);
+        return solveGreedy(nums);
     }
 };
