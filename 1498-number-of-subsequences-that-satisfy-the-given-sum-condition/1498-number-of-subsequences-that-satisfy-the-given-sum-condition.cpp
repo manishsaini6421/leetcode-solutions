19f1479d083037175ebc15b1e5f133d65ad1a483
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-06-29 11:52:00 +0530

    Time: 32 ms (52.57%), Space: 54.4 MB (14.79%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -8,14 +8,23 @@ public:
         if(b%2)return (temp*1l*temp*a)%MOD;
         return (temp*1l*temp)%MOD;
     }
+
     int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
+        int n=nums.size();
 
-        int i=0,j=nums.size()-1;
+        vector<int> dp(n+1,0);
+
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2; i<n; i++){
+            dp[i]=(dp[i-1]*2)%MOD;
+        }
+        int i=0,j=n-1;
         int ans=0;
         while(i<=j){
             if(nums[i]+nums[j]<=target){
-                ans= (ans + pow(2 ,j-i))%MOD;
+                ans= (ans + dp[j-i])%MOD;
                 i++;
             }
             else j--;
