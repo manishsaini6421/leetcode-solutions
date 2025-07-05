a5c65fb622a6afdeb5fb163422755aef18086bc8
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-07-05 23:00:55 +0530

    Time: 4 ms (23.15%), Space: 14.5 MB (55.09%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -3,13 +3,13 @@ public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         vector<int> limit(1001,0);
         for(auto trip:trips){
-            for(int i=trip[1]; i<trip[2]; i++){
-                limit[i]+=trip[0];
-            }
+            limit[trip[1]]+=trip[0];
+            limit[trip[2]]-=trip[0];
+            
         }
-        for(int i=0; i<=1000; i++){
-            if(limit[i]>capacity)return false;
+        for(int i=0;capacity>=0 && i<=1000; i++){
+            capacity-=limit[i];
         }
-        return true;
+        return capacity>=0;
     }
 };
