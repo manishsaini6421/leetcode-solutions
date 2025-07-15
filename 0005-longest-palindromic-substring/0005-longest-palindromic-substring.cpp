22f57c2e667b3a4185e666a3fd3b5823e04835e2
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-07-16 01:06:09 +0530

    Time: 272 ms (20%), Space: 292.8 MB (7.9%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -9,28 +9,62 @@ public:
         return dp[i][j]=0;
       
     }
-
-    string longestPalindrome(string s) {
-        
+    
+    pair<int,int> solve(string s){
         int n=s.size();
         int startIndex=0,maxLen=0;
         
 
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
         for(int i=0; i<n; i++){
-            dp[i][i]=1;
+            for(int j=i; j>=0; j--)
+            dp[i][j]=1;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=n-1; j>i; j--){
+                if(s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1];
+                } 
+                else dp[i][j]=0;
+            }
         }
 
         for(int i=0; i<n; i++){
             for(int j=i; j<n; j++){
-                if(isPalindrome(s,i,j,dp) && j-i+1>maxLen){
+                if(dp[i][j]==1 && j-i+1>maxLen){
                     maxLen=j-i+1;
                     startIndex=i;
                 }
             }
         }
         
-        return s.substr(startIndex,maxLen);
+        return {startIndex,maxLen};
+    }
+    string longestPalindrome(string s) {
+        
+        // int n=s.size();
+        // int startIndex=0,maxLen=0;
+        
+
+        // vector<vector<int>> dp(n,vector<int> (n,-1));
+
+        // for(int i=0; i<n; i++){
+        //     dp[i][i]=1;
+        // }
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         if(isPalindrome(s,i,j,dp) && j-i+1>maxLen){
+        //             maxLen=j-i+1;
+        //             startIndex=i;
+        //         }
+        //     }
+        // }
+        
+        // return s.substr(startIndex,maxLen);
+
+        pair<int,int> p=solve(s);
+        return s.substr(p.first,p.second);
     }
 };
