9a03bc61392eb4a37292b0f7c6a6931ab0c5bfa9
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-07-16 00:04:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.33%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,33 +1,11 @@
 class Solution {
 public:
-    bool check(string &sequence, string &word){
-        int n=word.size();
-        int ans=0;
-        for(int i=0; i<sequence.size(); i++){
-            cout<<i<<" ";
-            int count=0;
-            for(int j=0; j<n; j++){
-                if(sequence[i+j]==word[j])
-                {
-                    count++;
-                }
-                else
-                    break;
-                
-                
-            }
-            if(count==n){
-                return true;
-            }
-            
-        }
-        return false;
-    }
+   
     int maxRepeating(string sequence, string word) {
         
         int ans=0;
         string str=word;
-        while(check(sequence,word)){
+        while(sequence.find(word)!=string::npos){
             ans++;
             word+=str;
         }
