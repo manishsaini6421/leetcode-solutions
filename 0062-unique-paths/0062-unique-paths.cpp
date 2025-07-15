fe94b237a7a4395b7bf065260a13b4b0d117ffba
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-07-16 01:53:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -11,8 +11,30 @@ public:
 
         return dp[i][j]=left+top;
     }
-    int uniquePaths(int m, int n) {
+
+    int solveTab(int m,int n){
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        for(int i=0; i<m; i++){
+            dp[i][0]=1;
+        }
+        for(int j=0; j<n; j++){
+            dp[0][j]=1;
+        }
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                 int left=dp[i][j-1];
+                int top=dp[i-1][j];
+
+                dp[i][j]=left+top;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    int uniquePaths(int m, int n) {
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // return solve(m-1,n-1,dp);
+
+        return solveTab(m,n);
     }
 };
