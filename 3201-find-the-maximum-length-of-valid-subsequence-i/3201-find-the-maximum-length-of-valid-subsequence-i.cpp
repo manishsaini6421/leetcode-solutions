7208028362c89f24a1144611655d5f769ac678ea
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-07-16 10:44:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    
+    int maximumLength(vector<int>& nums) {
+        int odd=0,even=0;
+        for(int num:nums){
+            if(num%2)odd++;
+            else even++;
+        }
+
+        int flag=0,count=0;
+        if(nums[0]%2){
+            flag=1;
+            count++;
+        }
+
+        
+        for(int num:nums){
+            if(num%2==0 && flag==1){
+                flag=0;
+                count++;
+                
+            }
+            if(num%2==1 && flag==0){
+                flag=1;
+                count++;
+                cout<<num<<" ";
+            }
+        }
+        cout<<count;
+        return max(count,max(odd,even));
+    }
+};
