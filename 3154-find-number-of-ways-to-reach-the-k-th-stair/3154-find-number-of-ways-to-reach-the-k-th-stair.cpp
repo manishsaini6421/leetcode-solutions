46590df46c48199998909ac63b53a380d1ab1e6f
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-08-07 12:57:06 +0530

    Time: 102 ms (51.3%), Space: 30.4 MB (59.74%) - LeetHub

--- 3154-find-number-of-ways-to-reach-the-k-th-stair/3154-find-number-of-ways-to-reach-the-k-th-stair.cpp
+++ 3154-find-number-of-ways-to-reach-the-k-th-stair/3154-find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -1,45 +1,38 @@
-#define ll long long
-
-ll dp[50][50][2];
-
-ll find(int pos, int stat, int jump, int back, vector<ll> &exp, int k)
-{
-    if(pos > k+1) // no possible way to attain k once step number exceeds k + 1
-        return 0;
-    
-    if(dp[jump][back][stat] != -1)
-        return dp[jump][back][stat]; 
-    
-    ll ans = 0;
-    
-    if(pos == k) // dont end the recursion here as we may still reach k even after this
-        ++ans; 
-
-    if(stat == 1)
-    {
-        ans += find(pos-1, 0, jump, back+1, exp, k); // backstep
-        ans += find(pos+exp[jump], 1, jump+1, back, exp, k); // forward jump
-    }
-
-    if(stat == 0)
-        ans += find(pos+exp[jump], 1, jump+1, back, exp, k); // forward jump
-    
-    return dp[jump][back][stat] = ans;
-}
-
-
 class Solution {
 public:
-    int waysToReachStair(int k) {
-            
-        memset(dp, -1, sizeof(dp));
-        vector<ll> exp(33,0);
+
+    int solve(int i, int jump, int jumpType, int k,map<array<int,3>,int> &dp,vector<long long> &exp) {
+        if (i > k + 1 )
+            return 0;
+
+        int ans=0; 
+        if (i == k) {
+            ans++;
+        }
+
+        if(dp.count({i,jump,jumpType}))return dp[{i,jump,jumpType}];
+
         
-        for(int i=0; i<33; ++i) // creating a pre-computed array containing powers of 2 to speed up the calculation.
-            exp[i] = pow(2, i);
         
-        ll ans = find(1, 1, 0, 0, exp, k);  
-            
-        return ans;
+
+        if (jumpType && i>0)
+            ans += solve(i - 1, jump, 0, k,dp,exp);
+        
+            ans += solve(i + exp[jump], jump + 1, 1, k,dp,exp);
+
+        return dp[{i,jump,jumpType}]=ans;
     }
+
+    
+
+    int waysToReachStair(int k) {
+        map<array<int,3>,int> dp;
+
+        vector<long long> exp(33);
+        exp[0]=1;
+        for(int i=1; i<33; i++){
+            exp[i]=exp[i-1]*2;
+        }
+         return solve(1, 0, 1, k,dp,exp);
+          }
 };
