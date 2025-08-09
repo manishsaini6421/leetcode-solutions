0030be5d6c0bf513a63f8c161283f8758fd8c04d
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-08-09 11:52:41 +0530

    Time: 1 ms (45.29%), Space: 16.1 MB (74.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -24,19 +24,17 @@ public:
         int n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, 0));
         dp[m - 1][n - 1] = grid[m - 1][n - 1];
-        for(int i=m-2; i>=0; i--){
-            dp[i][n-1]=grid[i][n-1]+dp[i+1][n-1];
-        }
-        for(int j=n-2; j>=0; j--){
-            dp[m-1][j]=grid[m-1][j]+dp[m-1][j+1];
-        }
 
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                int right = grid[i][j] + dp[i][j + 1];
-                int down =  grid[i][j] + dp[i + 1][j];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int right = INT_MAX;
+                if (j < n - 1)
+                    right = grid[i][j] + dp[i][j + 1];
+                int down = INT_MAX;
+                if (i < m - 1)
+                    down = grid[i][j] + dp[i + 1][j];
 
-                dp[i][j] = min(right, down);
+                if(!(i==m-1 && j==n-1))dp[i][j] = min(right, down);
             }
         }
         return dp[0][0];
