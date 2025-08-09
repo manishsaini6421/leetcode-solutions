fa4b02b9d11ad20dfdf37dcf0279a4a4e364f312
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-08-09 11:52:58 +0530

    Time: 0 ms (100%), Space: 15.3 MB (91.45%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -22,22 +22,26 @@ public:
     int solveTab(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+        vector<int> curr(n,0);
+        vector<int> next(n,0);
+        next[n-1]=curr[n-1]=grid[m-1][n-1];
+
+        
 
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 int right = INT_MAX;
                 if (j < n - 1)
-                    right = grid[i][j] + dp[i][j + 1];
+                    right = grid[i][j] + curr[j + 1];
                 int down = INT_MAX;
                 if (i < m - 1)
-                    down = grid[i][j] + dp[i + 1][j];
+                    down = grid[i][j] + next[j];
 
-                if(!(i==m-1 && j==n-1))dp[i][j] = min(right, down);
+                if(!(i==m-1 && j==n-1))curr[j] = min(right, down);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
     int minPathSum(vector<vector<int>>& grid) {
         // int m = grid.size();
