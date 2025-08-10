febd396710a4634d374d3c4216c29728fd6c2f29
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-08-10 11:47:59 +0530

    Time: 35 ms (50%), Space: 36.6 MB (27.21%) - LeetHub

--- 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.cpp
+++ 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int sumCounts(vector<int>& nums) {
         int n=nums.size();
-        map<array<int,2>,int> result; 
+        int ans=0;
         for(int i=0; i<n; i++){
             map<int,int> count;
             for(int j=i; j<n; j++){
                 count[nums[j]]++;
-                result[{i,j}]=count.size();
+                int t=count.size();
+                ans+=(t*t);
 
             }
         }
-        int ans=0;
-        for(auto pair:result){
-            ans+=(pair.second*pair.second);
-        }
+        
         return ans;
     }
 };
