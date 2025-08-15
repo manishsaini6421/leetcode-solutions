39adf847fabc37cd7192dd619cac5342dee09653
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-08-15 07:22:54 +0530

    Time: 0 ms (100%), Space: 8 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==1)return true;
-        return n>0 && !(n%4)&& (int)pow(4,15)%n==0;
+        if(n<=0)return false;
+        double x=log10(n)/log10(4);
+        return fabs(x-round(x))<1e-10;
     }
 };
