aa28405e355fb1528f7db7d92834bb601e180e56
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-09-12 20:33:31 +0530

    Time: 18 ms (39.8%), Space: 15.6 MB (92.17%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string temp="";
+        map<char,int> mp;
         for(int i=0; i<s.size(); i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' ||s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
-                temp+=s[i];
+                mp[s[i]]++;
             }
 
         }
-        sort(temp.begin(),temp.end());
+        string temp="AEIOUaeiou";
         int index=0;
         for(int i=0; i<s.size(); i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' ||s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+                while(mp[temp[index]]==0){
+                    index++;
+                }
                 s[i]=temp[index];
-                index++;
+                mp[temp[index]]--;
             }
 
         }
