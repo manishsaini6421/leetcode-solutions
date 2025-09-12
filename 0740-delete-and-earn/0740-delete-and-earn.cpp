af0e0d4b6874f5d55a516f30fc1e5870568b1971
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-09-12 11:57:50 +0530

    Time: 7 ms (35.66%), Space: 19 MB (19.12%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &arr,int index,int tookPrev,vector<vector<int>> &dp){
-       if(index>=arr.size())return 0;
+    int solve(vector<int>& arr, int index, int tookPrev,
+              vector<vector<int>>& dp) {
+        if (index >= arr.size())
+            return 0;
 
-        if(dp[index][tookPrev]!=-1)return dp[index][tookPrev];
-        int take=0;
-        if(!tookPrev)take=arr[index]+solve(arr,index+1,1,dp);
+        if (dp[index][tookPrev] != -1)
+            return dp[index][tookPrev];
+        int take = 0;
+        if (!tookPrev)
+            take = arr[index] + solve(arr, index + 1, 1, dp);
 
-        int dontTake=solve(arr,index+1,0,dp);
+        int dontTake = solve(arr, index + 1, 0, dp);
 
-        return dp[index][tookPrev]=max(take,dontTake);
+        return dp[index][tookPrev] = max(take, dontTake);
     }
 
-    // int solveTab(vector<int> &arr){
+    int solveTab(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        for (int index = n - 1; index >= 0; index--) {
+            for (int tookPrev = 1; tookPrev >= 0; tookPrev--) {
+                int take = 0;
+                if (!tookPrev)
+                    take = arr[index] + solve(arr, index + 1, 1, dp);
 
-    // }
+                int dontTake = solve(arr, index + 1, 0, dp);
+
+                dp[index][tookPrev] = max(take, dontTake);
+            }
+        }
+        return dp[0][0];
+    }
     int deleteAndEarn(vector<int>& nums) {
-       
-       int maxElement= *max_element(nums.begin(),nums.end());
-       
-       vector<int> arr(maxElement+1,0);
 
-       int n=nums.size();
-        
-        for(int i=0; i<n; i++){
-            arr[nums[i]]+=nums[i];
+        int maxElement = *max_element(nums.begin(), nums.end());
+
+        vector<int> arr(maxElement + 1, 0);
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]] += nums[i];
         }
 
-        vector<vector<int>> dp(maxElement+1,vector<int>(2,-1)); 
-        int ans=solve(arr,0,0,dp);
-        return ans;
+        vector<vector<int>> dp(maxElement + 1, vector<int>(2, -1));
+         
+        return solve(arr, 0, 0, dp);;
     }
 };
