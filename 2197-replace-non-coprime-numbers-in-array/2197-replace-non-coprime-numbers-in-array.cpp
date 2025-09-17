4672a1d9d626eb6c31e30402852360b1ad37cb21
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-09-17 10:43:14 +0530

    Time: 31 ms (28.57%), Space: 124.8 MB (97.24%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-   
     vector<int> replaceNonCoprimes(vector<int>& nums) {
         stack<int> st;
         st.push(nums[0]);
-        for(int i=1; i<nums.size(); i++){
-            if(gcd(st.top(),nums[i])!=1){
-                
-                
-                int temp1=st.top();
+        for (int i = 1; i < nums.size(); i++) {
+            int val = nums[i];
+            
+            while (!st.empty() && gcd(st.top(), val) != 1) {
+                val = lcm(st.top(), val);
                 st.pop();
-               int temp2 = lcm(temp1,nums[i]);
-               while(!st.empty() && gcd(st.top(),temp2)!=1){
-                temp1=st.top();
-                st.pop();
-                temp2=lcm(temp1,temp2);
-               } 
-               st.push(temp2);
             }
-            else{
-                st.push(nums[i]);
-            }
+            st.push(val);
         }
-        vector<int> v(st.size(),0);
-        while(! st.empty()){
-            v[st.size()-1]=st.top();
+        vector<int> v(st.size(), 0);
+        while (!st.empty()) {
+            v[st.size() - 1] = st.top();
             st.pop();
         }
         return v;
