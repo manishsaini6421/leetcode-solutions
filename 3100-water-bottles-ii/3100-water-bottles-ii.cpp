a47efbed4a915717faf5cc66544294e2bd7ad69c
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-02 15:28:30 +0530

    Time: 6 ms (5.85%), Space: 8.7 MB (3.41%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.cpp
+++ 3100-water-bottles-ii/3100-water-bottles-ii.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+    // int maxBottlesDrunk(int numBottles, int numExchange) {
+    //     int ans=0;
+    //     while(numBottles>=numExchange){
+    //         ans+=numExchange;
+    //         numBottles-=numExchange;
+    //         numBottles+=1;
+    //         numExchange++;
+    //     }
+    //     ans+=numBottles;
+    //     return ans;
+    // }
     int maxBottlesDrunk(int numBottles, int numExchange) {
-        int ans=0;
-        while(numBottles>=numExchange){
-            ans+=numExchange;
-            numBottles-=numExchange;
-            numBottles+=1;
-            numExchange++;
-        }
-        ans+=numBottles;
-        return ans;
+        int a=1;
+        int b=2*numExchange -3;
+        double delta= (double)(b*b - 4 * a * (-2*numBottles));
+
+        double ans=ceil(((-b)+(double)sqrt(delta))/2);
+        return numBottles+ans-1;
     }
 };
