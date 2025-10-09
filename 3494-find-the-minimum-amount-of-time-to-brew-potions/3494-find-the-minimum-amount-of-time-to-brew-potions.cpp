6e3557d6de145c5c2b9110dab73be380d36519b5
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-09 12:42:30 +0530

    Time: 490 ms (53.03%), Space: 45.5 MB (43.43%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -1,19 +1,23 @@
+
 class Solution {
 public:
-    using ll = long long;
+    
     long long minTime(vector<int>& skill, vector<int>& mana) {
-        int n = skill.size(), m = mana.size();
-        vector<ll> times(n);
-        for (int j = 0; j < m; j++) {
-            ll cur_time = 0;
-            for (int i = 0; i < n; i++) {
-                cur_time = max(cur_time, times[i]) + skill[i] * mana[j];
+        int n=skill.size();
+        int m=mana.size();
+        vector<long long> time(n);
+       
+
+        for(int j=0; j<m; j++){
+            long long curr_time=0;
+            for(int i=0; i<n; i++){
+                curr_time=max(curr_time,time[i])+skill[i]*mana[j];
             }
-            times[n - 1] = cur_time;
-            for (int i = n - 2; i >= 0; i--) {
-                times[i] = times[i + 1] - skill[i + 1] * mana[j];
+            time[n-1]=curr_time;
+            for(int i=n-2; i>=0; i--){
+                time[i]=time[i+1]-skill[i+1]*mana[j];
             }
         }
-        return times[n - 1];
+        return time[n-1];
     }
 };
