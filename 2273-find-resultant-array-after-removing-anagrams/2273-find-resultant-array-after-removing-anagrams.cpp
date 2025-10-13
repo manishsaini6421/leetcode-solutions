fc4d18ba76f2884f5b2ee185c5086ed140784cbb
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-14 04:39:33 +0530

    Time: 7 ms (19.16%), Space: 18.6 MB (14.84%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -10,20 +10,19 @@ public:
 
         }
 
-
-        for(int i=0; i<words.size(); i++){
-            for(int j=i+1; j<words.size(); j++){
+        vector<string> ans;
+        for(int i=0; i<n; i++){
+            ans.push_back(words[i]);
+            for(int j=i+1; j<n; j++){
                 int count=0;
                 for(int k=0; k<26; k++){
                     if(freq[i][k]==freq[j][k])count++;
                 }
-                if(count==26){
-                    words.erase(words.begin()+j);
-                    freq.erase(freq.begin()+j);
-                    j--;
-                }
+                if(count!=26)break;
+                else i++;
             }
+            
         }
-        return words;
+        return ans;
     }
 };
