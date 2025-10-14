b9becaf313c96315c7bf15ef3296b0a5f8a93ed9
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-14 20:58:09 +0530

    Time: 2 ms (77.87%), Space: 7.7 MB (63.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,59 +1,56 @@
 class Solution {
 public:
-    vector<vector<int>> matmul(vector<vector<int>> &mt1,vector<vector<int>> &mt2){
-        int m=mt1.size();
-        int n=mt1[0].size();
-        int p=mt2[0].size();
-
-        vector<vector<int>> ans(m,vector<int> (p,0));
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<p; j++){
-                for(int k=0; k<n; k++){
-                    ans[i][j]+=mt1[i][k]*mt2[k][j];
-                }
-                
-            }
-        }
-        return ans;
+    //Using recursion
+    int solve(int n){
+        if(n<=1)return n;
+        return solve(n-1)+solve(n-2);
     }
-    vector<vector<int>> matrixExpo(vector<vector<int>> &mt,int n){
-        if(n==0){
-            vector<vector<int>> temp(mt.size(),vector<int>(mt.size(),0));
-            for(int i=0; i<mt.size(); i++){
-                temp[i][i]=1;
+    
+    // Using DP-recursion(Top-Down)
+    int solveMem(int n,vector<int> &dp){
+        if(n<=1)return n;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=solveMem(n-1,dp)+solveMem(n-2,dp);
+    }
 
-            }
-            return temp;
+    //Using DP-Tabulation(Bottom-UP)
+    int solveTab(int n){
+        vector<int> dp(n+1);
+        dp[0]=0;
+        if(n>0) dp[1]=1;
+
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(n==1)return mt;
-        vector<vector<int>> ans=matrixExpo(mt,n/2);
-        ans=matmul(ans,ans);
-        if(n%2)ans=matmul(ans,mt);
-        return ans;
+        return dp[n];
+
+    }
+
+    //Using DP-Optimization(Bottom-UP)
+    int solveOpt(int n){
+        if(n<=1)return n;
+        int prev=1,prev2=0;
+        
+        for(int i=2; i<=n; i++){
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
         
     }
+
     int fib(int n) {
-        // if(n<=1)return n;
-        // int temp1=0,temp2=1;
-        // for(int i=2; i<=n; i++){
-        //     int temp=temp2;
-        //     temp2=temp2+temp1;
-        //     temp1=temp;
-        // }
-        // return temp2;
+        // return solve(n);
 
-        if(n<=1)return n;
-        vector<vector<int>> mt(2,vector<int> (2,0));
-        mt[0][0]=1;
-        mt[0][1]=1;
-        mt[1][0]=1;
+        // vector<int> dp(n+1,-1);
+        // return solveMem(n,dp);
 
-        vector<vector<int>> temp(2,vector<int>(1,0));
-        temp[0][0]=1;
 
-        mt=matrixExpo(mt,n-1);
-        vector<vector<int>> ans=matmul(mt,temp);
-        return ans[0][0];
+        // return solveTab(n);
+
+
+        return solveOpt(n);
     }
+
 };
