a4941bb22387ff0952d3e5db0621155806f842a3
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-19 06:27:08 +0530

    Time: 102 ms (89.26%), Space: 66.6 MB (44.57%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
+    //Using minHeap
+    // int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    //     int n=arr.size();
+    //     unordered_map<int,int> mp;
+    //     vector<int> v;
+    //     for(int i=0; i<n; i++){
+    //         mp[arr[i]]++;
+    //     }
+    //     for(auto pair:mp){
+    //         v.push_back(pair.second);
+    //     }
+    //     priority_queue<int,vector<int>, greater<int>> pq(v.begin(),v.end());
+    //     while(k>0){
+    //         int temp=pq.top();
+            
+            
+    //         if(temp<=k){
+    //             k-=temp;
+    //             pq.pop();
+    //         }
+    //         else break;
+
+    //     }
+    //     return pq.size();
+
+    // }
+
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        int n=arr.size();
         unordered_map<int,int> mp;
-        vector<int> v;
-        for(int i=0; i<n; i++){
-            mp[arr[i]]++;
+        for(int num:arr){
+            mp[num]++;
         }
+        vector<int> freq;
         for(auto pair:mp){
-            v.push_back(pair.second);
+            freq.push_back(pair.second);
         }
-        priority_queue<int,vector<int>, greater<int>> pq(v.begin(),v.end());
-        while(k>0){
-            int temp=pq.top();
-            
-            
-            if(temp<=k){
-                k-=temp;
-                pq.pop();
+        sort(freq.begin(),freq.end());
+        
+        int count=freq.size();
+        int i=0;
+        for(int c:freq){
+            if(c<=k){
+                k-=c;
+                count--;
             }
             else break;
-
         }
-        return pq.size();
-
+        return count;
     }
 };
