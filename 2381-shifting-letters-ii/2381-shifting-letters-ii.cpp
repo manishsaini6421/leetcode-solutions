cf2825f74beb03a3e288f352cc73983802f2257c
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-23 03:43:18 +0530

    Time: 0 ms (100%), Space: 97.8 MB (85.91%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n=shifts.size();
-        int m=s.size();
-        vector<int> arr(m+1,0);
-       
-        for(int i=0; i<n; i++){
-            if(shifts[i][2]==0){
-                arr[shifts[i][0]]-=1;
-                arr[shifts[i][1]+1]+=1;
-            }
-            else{
+        int n=s.size();
+        int m=shifts.size();
+
+        vector<int> arr(n+1,0);
+        for(int i=0; i<m; i++){
+            if(shifts[i][2]==1){
                 arr[shifts[i][0]]+=1;
                 arr[shifts[i][1]+1]-=1;
             }
-            
+            else{
+                arr[shifts[i][0]]-=1;
+                arr[shifts[i][1]+1]+=1;
+            }
         }
-        int sum=0;
-        for(int i=0; i<m; i++){
-            sum+=arr[i];
-            arr[i]=sum;
+        for(int i=1; i<n; i++){
+            arr[i]+=arr[i-1];
         }
-        for(int i=0; i<m; i++){
+        for(int i=0; i<n; i++){
             int shift=arr[i]%26;
             if(shift<0)shift+=26;
-            s[i]='a'+(s[i]-'a'+shift)%26;
-            
+            s[i]=((s[i]-'a'+shift)%26)+'a';
         }
         return s;
     }
