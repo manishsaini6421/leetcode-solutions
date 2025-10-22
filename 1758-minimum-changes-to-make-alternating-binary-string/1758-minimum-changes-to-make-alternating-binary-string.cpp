e37544916a01cb99c9923b7457704c7c06f6ee71
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-23 03:02:07 +0530

    Time: 2 ms (25.51%), Space: 9.4 MB (78.36%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,14 +4,22 @@ public:
         int n=s.size();
         int zero=0,one=0;
         for(int i=0; i<n; i++){
-            if(i%2 && s[i]=='0')zero++;
-            if(i%2==0 && s[i]=='1')zero++;
+            if(i%2){
+                if(s[i]=='0')zero++;
+                else one++;
+            }
+            else{
+                if(s[i]=='1')zero++;
+                else one++;
+            }
+            // if(i%2 && s[i]=='0')zero++;
+            // if(i%2==0 && s[i]=='1')zero++;
         }
-        for(int i=0; i<n; i++){
-            if(i%2 && s[i]=='1')one++;
-            if(i%2==0 && s[i]=='0')one++;
-        }
-        cout<<one<<" "<<zero;
+        // for(int i=0; i<n; i++){
+        //     if(i%2 && s[i]=='1')one++;
+        //     if(i%2==0 && s[i]=='0')one++;
+        // }
+        // cout<<one<<" "<<zero;
         return min(zero,one);
     }
 };
