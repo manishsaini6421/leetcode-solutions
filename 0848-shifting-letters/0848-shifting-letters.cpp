9f4c723f123fe2d4ac0be86f5dba43e81691de9e
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-23 02:34:56 +0530

    Time: 2 ms (77.29%), Space: 71.8 MB (45.43%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.cpp
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -3,7 +3,7 @@ public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n=shifts.size();
         for(int i=n-2; i>=0; i--){
-            shifts[i]+=shifts[i+1];
+            shifts[i]= (shifts[i]+shifts[i+1])%26;
         }
         // reverse(shifts.begin(),shifts.end());
 
