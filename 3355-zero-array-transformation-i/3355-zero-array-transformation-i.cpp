a3619508aa4aec66092ff26f7e798e46821fc759
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-24 16:18:44 +0530

    Time: 811 ms (5.05%), Space: 292.7 MB (95.24%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int Q=queries.size();
+        int n=nums.size();
+        vector<int> diff(n+1,0);
+        
+        for(int i=0; i<Q; i++){
+            diff[queries[i][0]]+=1;
+            diff[queries[i][1]+1]-=1;
+        }
+        for(int i=1; i<n; i++){
+            diff[i]+=diff[i-1];
+        }
+        bool result=true;
+        for(int i=0; i<n; i++){
+            cout<<diff[i]<<" ";
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]-diff[i]>0)return false;
+        }
+        return result;
+    }
+};
