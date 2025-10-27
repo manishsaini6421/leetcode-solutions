04e47181686539213c279aa63711f9b049f1d080
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-27 21:15:21 +0530

    Time: 80 ms (5.13%), Space: 27.2 MB (95.94%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
+    // int numberOfBeams(vector<string>& bank) {
+    //     int m=bank.size();
+    //     int n=bank[0].size();
+
+    //     vector<int> count(m,0);
+    //     for(int i=0; i<m; i++){
+    //         int temp=0;
+    //         for(int j=0; j<n; j++){
+    //             if(bank[i][j]-'0'==1)temp++;
+    //         }
+    //         count[i]=temp;
+    //     }
+       
+    //     int ans=0;
+    //     for(int i=0; i<m-1; i++){
+    //         int j=i+1;
+    //         while(j<m-1 && count[j]==0){
+    //             j++;
+    //         }
+    //         ans+=(count[i]*count[j]);
+    //     }
+    //     return ans;
+    // }
+
+
+    //Optimized
     int numberOfBeams(vector<string>& bank) {
         int m=bank.size();
         int n=bank[0].size();
 
-        vector<int> count(m,0);
-        for(int i=0; i<m; i++){
-            int temp=0;
+        int next=0;
+        int ans=0;
+        for(int i=m-1; i>=0; i--){
+            int temp=0; 
             for(int j=0; j<n; j++){
                 if(bank[i][j]-'0'==1)temp++;
             }
-            count[i]=temp;
-        }
-       
-        int ans=0;
-        for(int i=0; i<m-1; i++){
-            int j=i+1;
-            while(j<m-1 && count[j]==0){
-                j++;
-            }
-            ans+=(count[i]*count[j]);
+            ans+=(temp*next);
+            next=temp?temp:next;
+            cout<<next<<" "<<ans<<endl;
         }
         return ans;
     }
