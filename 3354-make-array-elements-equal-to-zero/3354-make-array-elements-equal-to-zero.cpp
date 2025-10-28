e583f4920de6128cd794e027c45236c47a499215
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-28 22:55:46 +0530

    Time: 175 ms (17.89%), Space: 23.3 MB (22.02%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,46 +1,88 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        for (auto num : nums) {
-            if (num)
-                return false;
-        }
-        return true;
-    }
+    // bool check(vector<int>& nums) {
+    //     for (auto num : nums) {
+    //         if (num)
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    // int countValidSelections(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int ans = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         if (nums[i] == 0) {
+    //             int curr = i;
+    //             int dir = -1;
+    //             vector<int> temp = nums;
+    //             while (curr >= 0 && curr < n) {
+
+    //                 if (temp[curr] > 0) {
+    //                     temp[curr]--;
+    //                     dir = -dir;
+    //                 }
+    //                 curr += dir;
+    //             }
+    //             if (check(temp)) {
+    //                 ans++;
+    //             }
+
+    //             // right
+    //             curr = i;
+    //             temp = nums;
+    //             dir = 1;
+    //             while (curr >= 0 && curr < n) {
+
+    //                 if (temp[curr] > 0) {
+    //                     temp[curr]--;
+    //                     dir = -dir;
+    //                 }
+    //                 curr += dir;
+    //             }
+    //             if (check(temp))
+    //                 ans++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+    //Optimized
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                int curr = i;
-                int dir = -1;
-                vector<int> temp = nums;
-                while (curr >= 0 && curr < n) {
-
-                    if (temp[curr] > 0) {
+        int n=nums.size();
+        int count=0,ans=0;
+        for(int num:nums){
+            if(num)count++;
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                vector<int> temp=nums;
+                int curr=i;
+                int dir=1;
+                int tempCount=count;
+                while(curr>=0 && curr<n && tempCount!=0){
+                    if(temp[curr]){
                         temp[curr]--;
-                        dir = -dir;
+                        dir= -dir;
+                        if(temp[curr]==0)tempCount--;
                     }
-                    curr += dir;
+                    curr+=dir;
                 }
-                if (check(temp)) {
-                    ans++;
-                }
+                if(tempCount==0)ans++;
 
-                // right
-                curr = i;
-                temp = nums;
-                dir = 1;
-                while (curr >= 0 && curr < n) {
-
-                    if (temp[curr] > 0) {
+                temp=nums;
+                curr=i;
+                dir=-1;
+                tempCount=count;
+                while(curr>=0 && curr<n && tempCount!=0){
+                    if(temp[curr]){
                         temp[curr]--;
-                        dir = -dir;
+                        dir= -dir;
+                        if(temp[curr]==0)tempCount--;
                     }
-                    curr += dir;
+                    curr+=dir;
                 }
-                if (check(temp))
-                    ans++;
+                if(tempCount==0)ans++;
             }
         }
         return ans;
