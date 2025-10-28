7f22a6bdc3b4964e0acc6a106d567405b08b72b8
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2025-10-28 22:56:47 +0530

    Time: 6 ms (49.08%), Space: 21.8 MB (94.04%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -48,42 +48,61 @@ public:
 
 
     //Optimized
-    int countValidSelections(vector<int>& nums) {
-        int n=nums.size();
-        int count=0,ans=0;
-        for(int num:nums){
-            if(num)count++;
-        }
-        for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                vector<int> temp=nums;
-                int curr=i;
-                int dir=1;
-                int tempCount=count;
-                while(curr>=0 && curr<n && tempCount!=0){
-                    if(temp[curr]){
-                        temp[curr]--;
-                        dir= -dir;
-                        if(temp[curr]==0)tempCount--;
-                    }
-                    curr+=dir;
-                }
-                if(tempCount==0)ans++;
+    // int countValidSelections(vector<int>& nums) {
+    //     int n=nums.size();
+    //     int count=0,ans=0;
+    //     for(int num:nums){
+    //         if(num)count++;
+    //     }
+    //     for(int i=0; i<n; i++){
+    //         if(nums[i]==0){
+    //             vector<int> temp=nums;
+    //             int curr=i;
+    //             int dir=1;
+    //             int tempCount=count;
+    //             while(curr>=0 && curr<n && tempCount!=0){
+    //                 if(temp[curr]){
+    //                     temp[curr]--;
+    //                     dir= -dir;
+    //                     if(temp[curr]==0)tempCount--;
+    //                 }
+    //                 curr+=dir;
+    //             }
+    //             if(tempCount==0)ans++;
 
-                temp=nums;
-                curr=i;
-                dir=-1;
-                tempCount=count;
-                while(curr>=0 && curr<n && tempCount!=0){
-                    if(temp[curr]){
-                        temp[curr]--;
-                        dir= -dir;
-                        if(temp[curr]==0)tempCount--;
-                    }
-                    curr+=dir;
-                }
-                if(tempCount==0)ans++;
+    //             temp=nums;
+    //             curr=i;
+    //             dir=-1;
+    //             tempCount=count;
+    //             while(curr>=0 && curr<n && tempCount!=0){
+    //                 if(temp[curr]){
+    //                     temp[curr]--;
+    //                     dir= -dir;
+    //                     if(temp[curr]==0)tempCount--;
+    //                 }
+    //                 curr+=dir;
+    //             }
+    //             if(tempCount==0)ans++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    //Most Optimized
+    int countValidSelections(vector<int>& nums) {
+        
+        int ans=0,currSum=0,totalSum=0;
+        for(int num:nums){
+            totalSum+=num;
+        }
+        for(int num:nums){
+            if(num==0){
+                int leftSum=currSum;
+                int rightSum=totalSum-currSum;
+                if(leftSum==rightSum)ans+=2;
+                else if(abs(leftSum-rightSum)==1)ans+=1;
             }
+            currSum+=num;
         }
         return ans;
     }
