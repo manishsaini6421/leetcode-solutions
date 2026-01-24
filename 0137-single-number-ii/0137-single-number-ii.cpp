d59f0b3a2ce987785f49a32f060b709975584d78
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2026-01-24 21:26:13 +0530

    Time: 0 ms (100%), Space: 13.3 MB (86.2%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    // int singleNumber(vector<int>& nums) {
-    //     int n=nums.size();
-    //     sort(nums.begin(),nums.end());
-    //     int count=1,x=nums[0];
-    //     for(int i=1; i<n; i++){
-    //         if(nums[i]!=x){
-    //             if(count==1)
-    //                 return x;
-    //             x=nums[i];
-    //             count=1;    
-    //         }
-    //         else count++;
-    //     }
-    //     return nums[n-1];
+   /* int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int count=1,x=nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i]!=x){
+                if(count==1)
+                    return x;
+                x=nums[i];
+                count=1;    
+            }
+            else count++;
+        }
+        return nums[n-1];
 
-    // }
+    } */
      
 
     //Using Bit Manipulation 
-    int singleNumber(vector<int>& nums) {
+    //at every place in binary representation 1's count should be k(i.e. 3)
+    //if k%3!=0 then relsut bit set to 1 on that place.
+    // T(n)=N*32
+    //SC=O(1)
+    /* int singleNumber(vector<int>& nums) {
         int count=0;
         int n=nums.size();
         int num=0;
@@ -34,6 +38,22 @@ public:
       
         
         return num;
+    } */
+    
+    //Using Sorting
+    //TC=NlogN <= N*32 because logN=32 when N is 2^32
+    //SC = O(1)
+    //So this is better solution
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=1; i<n; i+=3){
+            if(nums[i]!=nums[i-1])return nums[i-1];
+            if(i<n-1 && nums[i]!=nums[i+1])return nums[i+1];
+        }
+        return nums[n-1];
     }
+
+
 
 };
