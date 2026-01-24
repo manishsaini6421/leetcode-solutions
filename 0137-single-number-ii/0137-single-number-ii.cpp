e1f474cf5bebf074038fde745d1982d75c8094a5
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2026-01-24 21:11:40 +0530

    Time: 0 ms (100%), Space: 13.3 MB (64.18%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int count=1,x=nums[0];
-        for(int i=1; i<n; i++){
-            if(nums[i]!=x){
-                if(count==1)
-                    return x;
-                x=nums[i];
-                count=1;    
-            }
-            else count++;
-        }
-        return nums[n-1];
+    // int singleNumber(vector<int>& nums) {
+    //     int n=nums.size();
+    //     sort(nums.begin(),nums.end());
+    //     int count=1,x=nums[0];
+    //     for(int i=1; i<n; i++){
+    //         if(nums[i]!=x){
+    //             if(count==1)
+    //                 return x;
+    //             x=nums[i];
+    //             count=1;    
+    //         }
+    //         else count++;
+    //     }
+    //     return nums[n-1];
 
+    // }
+     
+
+    //Using Bit Manipulation 
+    int singleNumber(vector<int>& nums) {
+        int A[32]={0};
+        int n=nums.size();
+        for(int i=0; i<32; i++){
+            for(int j=0; j<n; j++){
+                A[31-i]+=(nums[j]&1);
+                nums[j]>>=1;
+            }
+        }
+        int num=0;
+        long pow=1;
+        for(int i=31; i>=0; i--){
+            num+=(A[i]%3)*pow;
+            pow*=2;
+        }
+        return num;
     }
+
 };
