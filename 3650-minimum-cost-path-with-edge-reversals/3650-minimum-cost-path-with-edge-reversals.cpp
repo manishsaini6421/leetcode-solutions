4d4c9ac632b2c99f2f9c7d32c873c2101fcb927a
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2026-01-28 01:21:43 +0530

    Time: 429 ms (23.65%), Space: 346.5 MB (17.56%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int,int>>> adjList(n);
+        vector<vector<pair<int, int>>> adjList(n);
         for (auto edge : edges) {
             adjList[edge[0]].push_back({edge[1], edge[2]});
             adjList[edge[1]].push_back({edge[0], 2 * edge[2]});
         }
-        vector<int> visited(n, 0);
         vector<int> dist(n, INT_MAX);
         dist[0] = 0;
         set<pair<int, int>> s;
@@ -16,18 +15,16 @@ public:
             auto top = *s.begin();
             int pathLength = top.first;
             int node = top.second;
-            visited[node] = 1;
             s.erase(s.begin());
             for (auto pair : adjList[node]) {
                 int neighbour = pair.first;
                 int edgeWeight = pair.second;
-                if (!visited[neighbour]) {
+               
                     if (dist[neighbour] > edgeWeight + pathLength) {
                         dist[neighbour] = edgeWeight + pathLength;
                         s.insert({dist[neighbour], neighbour});
                     }
-                    
-                }
+                
             }
         }
         return (dist[n - 1] != INT_MAX) ? dist[n - 1] : -1;
