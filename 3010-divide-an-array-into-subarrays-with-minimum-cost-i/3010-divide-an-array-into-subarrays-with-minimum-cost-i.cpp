d64a0cce053957c3c90ff56edc4a95e1987315d4
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2026-02-02 18:32:27 +0530

    Time: 0 ms (100%), Space: 30.9 MB (5.05%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int minimumCost(vector<int>& nums) {
+    // int minimumCost(vector<int>& nums) {
+    //     int ans=nums[0];
+    //     int first=nums[1],second=INT_MAX,index=1;
+    //     for(int i=1; i<nums.size(); i++){
+    //         if(nums[i]<first){
+    //             first=nums[i];
+    //             index=i;
+    //         }
+    //     }
+    //     for(int i=1; i<nums.size(); i++){
+    //         if(nums[i]<second && i!=index){
+    //             second=nums[i];
+    //         }
+    //     }
+    //     return ans+first+second;
+    // }
+
+
+    int minimumCost(vector<int>& nums){
+        priority_queue<int,vector<int> ,greater<>> pq(nums.begin()+1,nums.end());
+        
         int ans=nums[0];
-        int first=nums[1],second=INT_MAX,index=1;
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]<first){
-                first=nums[i];
-                index=i;
-            }
-        }
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]<second && i!=index){
-                second=nums[i];
-            }
-        }
-        return ans+first+second;
+        ans+=pq.top();
+        pq.pop();
+        ans+=pq.top();
+        return ans;
     }
 };
