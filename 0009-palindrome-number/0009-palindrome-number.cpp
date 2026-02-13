2906ad28f65f0d66199863f534836a892e56100b
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2026-02-13 22:43:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
+    // bool isPalindrome(int x) {
+    //     if(x<0)return false;
+    //     string s="";
+    //     int num=x;
+    //     while(num){
+    //         s+=(char(num%10));
+    //         num/=10;
+    //     }
+
+    //     int i=0,j=s.size()-1;
+    //     while(i<=j){
+    //         if(s[i]!=s[j])return false;
+    //         i++;
+    //         j--;
+            
+    //     }
+    //     return true;
+    // }
     bool isPalindrome(int x) {
         if(x<0)return false;
-        string s="";
         int num=x;
-        while(num){
-            s+=(char(num%10));
-            num/=10;
-        }
 
-        int i=0,j=s.size()-1;
-        while(i<=j){
-            if(s[i]!=s[j])return false;
-            i++;
-            j--;
-            
+        int y=0;
+        while(x){
+            y=y*1l*10 + (x%10);
+            x/=10;
         }
-        return true;
+        return y==num;
+        
     }
 };
