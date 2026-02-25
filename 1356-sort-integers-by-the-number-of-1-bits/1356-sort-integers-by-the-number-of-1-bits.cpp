e4492ec7691b370177f2be6f126f344660bcfece
MANISH <57342164+manishsaini6421@users.noreply.github.com>
2026-02-25 17:14:20 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.86%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
    static bool comp(int a, int b){
-        int c1=0,c2=0,a1=a,b1=b;
-        while(a){
-            c1+= 1&a;
-            a>>=1;
-        }
-        while(b){
-            c2+= 1&b;
-            b>>=1;
-        }
-        if(c1<c2) return true;
-        else if(c1==c2 && a1<b1)return true;
+        if(__builtin_popcount(a)<__builtin_popcount(b))return true;
+        else if(__builtin_popcount(a)==__builtin_popcount(b) && a<b)return true;
+       
         return false;
     }
     vector<int> sortByBits(vector<int>& arr) {
